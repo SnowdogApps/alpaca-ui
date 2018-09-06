c03075bac25d5dcaf49710a494c771dc949f4b82
Bartek Igielski <igloczek@gmail.com>
2018-09-06 18:36:44 +0200

    Icon component migrated

---
+++ components/01-globals/icon/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg class="icon" role="img">
+    <use :xlink:href="url"></use>
+  </svg>
+</template>
+
+<script>
+  export default {
+    props: ['icon'],
+    computed: {
+      url() {
+        return `/icons-sprite.svg#${this.icon}`
+      }
+    }
+  }
+</script>
+
+<style>
+  .icon {
+    display: block;
+    width: var(--icon__size);
+    height: var(--icon__size);
+    padding: var(--icon__padding);
+  }
+</style>
---
+++ stories/01-global-icon.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/vue'
+
+import App from '../components/01-globals/app/App.vue'
+import Icon from '../components/01-globals/icon/Icon.vue'
+
+storiesOf('Global|Icon', module)
+  .add('Default', () => ({
+    components: { App, Icon },
+    template: `
+      <app>
+        <icon icon="arrow-down" />
+      </app>
+    `,
+  }))
