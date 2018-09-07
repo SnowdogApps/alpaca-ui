a06a9d49d4746b1b06bc6b993c6431f453662d3b
Bartek Igielski <igloczek@gmail.com>
2018-09-07 02:07:16 +0200

    Typography migrated

---
+++ stories/01-global-typography.stories.js
@@ -0,0 +1,50 @@
+import { storiesOf } from '@storybook/vue'
+
+import App from '../components/01-globals/app/App.vue'
+import AlpacaHeading from '../components/01-globals/typography/Heading.vue'
+import AlpacaLabel from '../components/01-globals/typography/Label.vue'
+import AlpacaLink from '../components/01-globals/typography/Link.vue'
+import AlpacaParagraph from '../components/01-globals/typography/Paragraph.vue'
+
+storiesOf('Global|Typography', module)
+  .add('Heading', () => ({
+    components: { App, AlpacaHeading },
+    template: `
+      <app>
+        <alpaca-heading level="1">H1</alpaca-heading>
+        <alpaca-heading level="4" tag="span">Span level 4</alpaca-heading>
+        <alpaca-heading level="6" :page="true">Page level 6</alpaca-heading>
+      </app>
+    `,
+  }))
+  .add('Label', () => ({
+    components: { App, AlpacaLabel },
+    template: `
+      <app>
+        <alpaca-label>Test</alpaca-label>
+        <alpaca-label :inline="true">Test</alpaca-label>
+        <alpaca-label :hidden="true">Test</alpaca-label>
+        <alpaca-label tag="span">Test</alpaca-label>
+      </app>
+    `,
+  }))
+  .add('Link', () => ({
+    components: { App, AlpacaLink },
+    template: `
+      <app>
+        <alpaca-link href="http://google.com/">Test</alpaca-link>
+        <alpaca-link href="http://google.com/" :invert="true">Test</alpaca-link>
+        <alpaca-link href="http://google.com/" :secondary="true">Test</alpaca-link>
+        <alpaca-link tag="span">Test</alpaca-link>
+      </app>
+    `,
+  }))
+  .add('Paragraph', () => ({
+    components: { App, AlpacaParagraph },
+    template: `
+      <app>
+        <alpaca-paragraph>Test</alpaca-paragraph>
+        <alpaca-paragraph tag="div">Test</alpaca-paragraph>
+      </app>
+    `,
+  }))
