fb848efb66c8885c8e08ff01dbd667aa596d5d42
Bartek Igielski <igloczek@gmail.com>
2018-09-08 23:19:31 +0200

    Addons cleanup. NPM package conifg.

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,25 +2,12 @@
 import { configure } from '@storybook/vue';
 import { setOptions } from '@storybook/addon-options';
 
-// // Vue
-// import Vue from 'vue';
-// import Vuex from 'vuex';
-
-// // Install Vue plugins
-// Vue.use(Vuex);
-
-// // Import and register components
-// import App from '../components/01-globals/app/App.vue';
-
-// Vue.component('app', App)
-
 setOptions({
   hierarchyRootSeparator: /\|/,
 });
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
+  const req = require.context('../stories', true, /.stories.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
