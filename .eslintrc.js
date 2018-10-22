2f65d802597c6d36b515fc0e1373e43d49295a19
Joanna Szczęch <joanna.szczech@snow.dog>
2018-10-22 11:03:07 +0200

    #46499  - Remove unnecessary rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     "array-bracket-spacing": 0,
     "computed-property-spacing": 0,
     "key-spacing": 0,
-    "no-var": 2,
-    "allowSingleLine": false
+    "no-var": 2
   }
 }
