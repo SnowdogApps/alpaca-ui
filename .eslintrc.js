b701169fff3152b76c08807ad1c5e36b180295b7
Joanna Szczęch <joanna.szczech@snow.dog>
2018-10-22 10:42:31 +0200

    #46499  - Eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    "plugin:vue/essential",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "no-multi-spaces": 0,
+    "space-in-parens": 0,
+    "array-bracket-spacing": 0,
+    "computed-property-spacing": 0,
+    "key-spacing": 0,
+    "no-var": 2,
+    "allowSingleLine": false
+  }
+}
