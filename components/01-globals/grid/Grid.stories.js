d0e62364562ccbf37b32d0374babaff8065a7b6d
Joanna Szczęch <joanna.szczech@snow.dog>
2018-10-31 21:32:36 +0100

    #46955 - Fix misspelling

--- components/01-globals/grid/Grid.stories.js
+++ components/01-globals/grid/Grid.stories.js
@@ -2,31 +2,31 @@ import {storiesOf} from '@storybook/vue'
 
 import App from '../app/App.vue'
 import GridContainer from './grid-container/GridContainer.vue'
-import GridCollumn from './grid-collumn/GridCollumn.vue'
+import GridColumn from './grid-column/GridColumn.vue'
 
 storiesOf('Global/Grid', module).add('Default', () => ({
-  components: {App, GridContainer, GridCollumn},
+  components: {App, GridContainer, GridColumn},
   template: `
       <app>
         <grid-container>
-          <grid-collumn width="1-2">
+          <grid-column width="1-2">
             1-2
-          </grid-collumn>
-          <grid-collumn width="1-2">
+          </grid-column>
+          <grid-column width="1-2">
             1-2
-          </grid-collumn>
+          </grid-column>
         </grid-container>
 
         <grid-container>
-          <grid-collumn width="1-3">
+          <grid-column width="1-3">
             1-3
-          </grid-collumn>
-          <grid-collumn width="1-3">
+          </grid-column>
+          <grid-column width="1-3">
             1-3
-          </grid-collumn>
-          <grid-collumn width="1-3">
+          </grid-column>
+          <grid-column width="1-3">
             1-3
-          </grid-collumn>
+          </grid-column>
         </grid-container>
       </app>
     `
