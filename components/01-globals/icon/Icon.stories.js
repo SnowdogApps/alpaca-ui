6c92401bf0f52aad066b18c2baf57f82c357ead2
Bartek Igielski <igloczek@gmail.com>
2018-11-21 10:10:45 +0100

    #47374 - Separate Icons preview component moved to story

--- components/01-globals/icon/Icon.stories.js
+++ components/01-globals/icon/Icon.stories.js
@@ -1,14 +1,39 @@
-import {storiesOf} from '@storybook/vue'
+import { storiesOf } from '@storybook/vue'
+import { withReadme } from 'storybook-readme';
+
+import iconsReadme from './README.md';
+import icons from './mocks/icons.json'
 
 import App from '../app/App.vue'
 import Icon from './Icon.vue'
 
 storiesOf('Global/Icon', module)
+  .addDecorator(withReadme(iconsReadme))
   .add('Default', () => ({
     components: { App, Icon },
+    data() {
+      return  {
+        icons
+      }
+    },
     template: `
       <app>
-        <icon icon="arrow-down" />
+        <div style="display: flex; flex-wrap: wrap; padding: 10px;">
+          <div
+            v-for="icon in icons"
+            :key="icon.iconId"
+            style="margin: 20px; width: 100px;"
+          >
+            <icon
+              :icon="icon.iconId"
+              :title="icon.iconTitle"
+              style="margin: 0 auto;"
+            />
+            <span style="display: block; margin-top: 20px; text-align: center;">
+              {{ icon.iconId }}
+            </span>
+          </div>
+        </div>
       </app>
     `
   }))
