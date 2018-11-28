6f779ed335d0979e87ca9b7fff9e2e50143dfac5
Robert Wozniak <30414211+robwozniak@users.noreply.github.com>
2018-11-28 19:08:47 +0100

    #35 - Update list element stories file. Add mocks with sample data

--- components/02-elements/list/List.stories.js
+++ components/02-elements/list/List.stories.js
@@ -4,306 +4,189 @@ import AlpacaList from './List.vue'
 import AlpacaImageList from './ImageList.vue'
 import AlpacaDescriptionList from './DescriptionList.vue'
 
+import listElements from './mocks/listElements.json'
+import iconListElements from './mocks/iconListElements.json'
+import imageListElements from './mocks/imageListElements.json'
+import descListElements from './mocks/descListElements.json'
+import linkListElements from './mocks/linkListElements.json'
+
 storiesOf('Elements/List', module)
   .add('Default', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
-        <alpaca-list
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
-        />
+        <alpaca-list :elements="listElements"/>
       </app>
     `
   }))
   .add('Native', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--native'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Horizontal', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--horizontal'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Horizontal Medium', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--horizontal@medium'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Icon', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      iconListElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--with-icon'"
-          :elements="[
-            {
-              icon: {
-                iconId: 'facebook'
-              },
-              linkHref: '#',
-              wrapperClass: 'list__icon-link'
-            },
-            {
-              icon: {
-                iconId: 'twitter'
-              },
-              linkHref: '#',
-              wrapperClass: 'list__icon-link'
-            },
-            {
-              icon: {
-                iconId: 'linkedin'
-              },
-              linkHref: '#',
-              wrapperClass: 'list__icon-link'
-            }
-          ]"
+          :elements="iconListElements"
         />
       </app>
     `
   }))
   .add('Image', () => ({
     components: { App, AlpacaImageList },
+    data: () => ({
+      imageListElements
+    }),
     template: `
       <app>
         <alpaca-image-list
           :listClass="'list--with-image'"
-          :elements="[
-            {
-              image: {
-                src: 'https://via.placeholder.com/16',
-                alt: 'Sample image'
-              },
-              text: 'Lorem Ipsum'
-            },
-            {
-              image: {
-                src: 'https://via.placeholder.com/16',
-                alt: 'Sample image'
-              },
-              text: 'Lorem Ipsum'
-            },
-            {
-              image: {
-                src: 'https://via.placeholder.com/16',
-                alt: 'Sample image'
-              },
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="imageListElements"
         />
       </app>
     `
   }))
   .add('Divided', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--divided'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Center', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--center'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Center Horizontal', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--center list--horizontal'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Divided Horizontal', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--divided list--horizontal'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Divided Horizontal Medium', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      listElements
+    }),
     template: `
       <app>
         <alpaca-list
           :listClass="'list--divided list--horizontal@medium'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            },
-            {
-              text: 'Lorem Ipsum'
-            }
-          ]"
+          :elements="listElements"
         />
       </app>
     `
   }))
   .add('Description', () => ({
     components: { App, AlpacaDescriptionList },
+    data: () => ({
+      descListElements
+    }),
     template: `
       <app>
         <alpaca-description-list
           :listClass="'list--divided'"
           :title="'Alpaca description List'"
-          :elements="[
-            {
-              label: 'Lorem',
-              value: 'ipsum'
-            },
-            {
-              label: 'Lorem',
-              value: 'ipsum'
-            },
-            {
-              label: 'Lorem',
-              value: 'ipsum'
-            }
-          ]"
+          :elements="descListElements"
         />
       </app>
     `
   }))
   .add('Link', () => ({
     components: { App, AlpacaList },
+    data: () => ({
+      linkListElements
+    }),
     template: `
       <app>
         <alpaca-list 
           :elementType="'link'"
-          :elements="[
-            {
-              text: 'Lorem Ipsum',
-              linkHref: '#'
-            },
-            {
-              text: 'Lorem Ipsum',
-              linkHref: '#'
-            },
-            {
-              text: 'Lorem Ipsum',
-              linkHref: '#'
-            }
-          ]"
+          :elements="linkListElements"
         />
       </app>
     `
