48822ee2ad4f3b51b2d4956809cd6987c87cfd3a
Joanna Szczęch <j_szczech@outlook.com>
2018-11-28 16:32:01 +0100

    #47808 - Remove semicolons

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,4 @@
 import '@storybook/addon-storysource/register'
 import '@storybook/addon-options/register'
 import '@storybook/addon-viewport/register'
-import '@storybook/addon-actions/register';
+import '@storybook/addon-actions/register'
