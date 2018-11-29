dd1975a06697ab524100127852742d70ad01ef2b
Robert Wozniak <30414211+robwozniak@users.noreply.github.com>
2018-11-29 15:36:32 +0100

    #35 list element (#59)

---
+++ components/02-elements/list/DescriptionList.vue
@@ -0,0 +1,59 @@
+<template>
+  <dl
+    :class="[
+      'list',
+      'list--description',
+      listClass
+    ]"
+  >
+    <template v-for="(item, idx) in elements">
+      <dt 
+        :key="`desc-list-label-${idx}`"
+        :class="[
+          'list__label',
+          labelClass
+        ]"
+      >
+        {{ item.label }}
+      </dt>
+      <dd
+        :key="`desc-list-value-${idx}`"
+        :class="[
+          'list__value',
+          valueClass
+        ]"
+      >
+        {{ item.value }}
+      </dd>
+    </template>        
+  </dl>
+</template>
+
+<script>
+export default {
+  props: {
+    elements: {
+      type: Array,
+      required: true
+    },
+    listClass: {
+      type: String,
+      default: null
+    },
+    labelClass: {
+      type: String,
+      default: null
+    },
+    valueClass: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .list--description {
+    display: inline-block;
+  }
+</style>
