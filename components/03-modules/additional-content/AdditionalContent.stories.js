8a21aaf909d1bc3bad9d79a29f703dfd3acd6ac1
Joanna Szczęch <joanna.szczech@snow.dog>
2018-12-05 12:21:40 +0100

    #48358 - Migrate additional-content

---
+++ components/03-modules/additional-content/AdditionalContent.stories.js
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/vue'
+
+import App from '../../01-globals/app/App.vue'
+import AlpacaAdditionalContent from './AdditionalContent.vue'
+
+import additionalContent from './mocks/additional-content.json'
+
+storiesOf('Modules/AdditionalContent', module)
+  .add('Default', () => ({
+    components: { App, AlpacaAdditionalContent },
+    data: () => ({
+      additionalContent
+    }),
+    template: `
+      <app>
+        <alpaca-additional-content 
+        :images="additionalContent.images"
+        :alt="additionalContent.images[0].alt"
+        :firstSection="additionalContent.firstSection.text"
+        :secondSection="additionalContent.secondSection.text">
+        </alpaca-additional-content>
+      </app>
+    `
+  }))
---
+++ components/03-modules/additional-content/AdditionalContent.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="additional-content">
+    <alpaca-picture
+      class="additional-content__image"
+      :images="images"
+      :alt="alt"
+    />
+
+    <div class="additional-content__info">
+      <div class="additional-content__section">
+        <alpaca-paragraph>
+          {{ firstSection }}
+        </alpaca-paragraph>
+      </div>
+
+      <div class="additional-content__section">
+        <alpaca-paragraph>
+          {{ secondSection }}
+        </alpaca-paragraph>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import AlpacaPicture from '../../03-modules/picture/Picture.vue'
+  import AlpacaParagraph from '../../01-globals/paragraph/Paragraph.vue'
+
+  export default {
+    components: {
+      AlpacaPicture,
+      AlpacaParagraph
+    },
+    props: {
+      images: {
+        type: Array,
+        required: true
+      },
+      alt: {
+        type: String,
+        required: true
+      },
+      firstSection: {
+        type: String,
+        default: null
+      },
+      secondSection: {
+        type: String,
+        default: null
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+  $additional-content__image-margin          : 0 0 41px !default;
+  $additional-content__image-margin\@large   : 0 0 32px !default;
+  $additional-section-width                  : 48% !default;
+
+  .additional-content {
+    &__image {
+      margin: $additional-content__image-margin;
+      @include mq($screen-l) {
+        margin: $additional-content__image-margin\@large;
+      }
+    }
+    &__info {
+      @include mq($screen-l) {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+      }
+
+    }
+    &__section {
+      @include mq($screen-l) {
+        flex: 0 0 $additional-section-width;
+      }
+    }
+  }
+</style>
