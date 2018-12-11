11a81acb6611b2059f7ca36775799aa1bd171e2b
Bartek Igielski <igloczek@gmail.com>
2018-12-11 04:17:08 +0100

    Update Label.stories.js

--- components/01-globals/label/Label.stories.js
+++ components/01-globals/label/Label.stories.js
@@ -7,33 +7,33 @@ storiesOf('Global/Label', module)
   .add('Default', () => ({
     components: { App, AlpacaLabel },
     template: `
-    <app>
-      <alpaca-label>
-        Label
-      </alpaca-label>
-    </app>
-  `
+      <app>
+        <alpaca-label>
+          Label
+        </alpaca-label>
+      </app>
+    `
   }))
   .add('Custom tag', () => ({
     components: { App, AlpacaLabel },
     template: `
-    <app>
-      <alpaca-label
-        tag="span"
-        customClass="custom"
-      >
-        Custom tag label
-      </alpaca-label>
-    </app>
-  `
+      <app>
+        <alpaca-label
+          tag="span"
+          customClass="custom"
+        >
+          Custom tag label
+        </alpaca-label>
+      </app>
+    `
   }))
   .add('Inline', () => ({
     components: { App, AlpacaLabel },
     template: `
-    <app>
-      <alpaca-label inline>
-        Inline label
-      </alpaca-label>
-    </app>
-  `
+      <app>
+        <alpaca-label inline>
+          Inline label
+        </alpaca-label>
+      </app>
+    `
   }))
