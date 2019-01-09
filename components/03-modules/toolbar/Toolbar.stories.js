b51512549c07d925157310916f467a35e05a6681
Joanna Szczęch <joanna.szczech@snow.dog>
2019-01-09 19:44:02 +0100

    #49080 - Migrate toolbar

---
+++ components/03-modules/toolbar/Toolbar.stories.js
@@ -0,0 +1,54 @@
+import { storiesOf } from '@storybook/vue';
+
+import toolbar from './mocks/toolbar.json'
+
+import App from '../../01-globals/app/App.vue';
+import AlpacaToolbar from './Toolbar.vue';
+
+storiesOf('Modules/Toolbar', module).add('Default', () => ({
+    data: () => {
+      return {
+        toolbar
+      }
+    },
+    components: { App, AlpacaToolbar },
+    template: `
+      <app>
+        <alpaca-toolbar
+          showMode
+          showSorter
+          showLimit
+          showAmount
+          :sortBy="toolbar.sortBy"
+          :limiter="toolbar.limiter"
+          :amount="toolbar.amount"
+        />
+      </app>
+    `
+  }))
+  .add('With pager', () => ({
+    data: () => {
+      return {
+        toolbar
+      }
+    },
+    components: { App, AlpacaToolbar },
+    template: `
+      <app>
+        <alpaca-toolbar
+          showMode
+          showSorter
+          showLimit
+          showPager
+          showAmount
+          gridHref="#"
+          listHref="#"
+          :sortBy="toolbar.sortBy"
+          :limiter="toolbar.limiter"
+          :pager="toolbar.pager"
+          :amount="toolbar.amount"
+        />
+      </app>
+    `
+  }))
+
