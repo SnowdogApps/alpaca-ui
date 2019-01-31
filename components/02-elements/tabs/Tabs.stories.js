09d55d95880ad619af4b586e25eb642c2364df20
Joanna Szczęch <joanna.szczech@snow.dog>
2019-01-31 15:54:54 +0000

    #48120 -  Rewrite tabs component

--- components/02-elements/tabs/Tabs.stories.js
+++ components/02-elements/tabs/Tabs.stories.js
@@ -1,41 +1,86 @@
 import { storiesOf } from '@storybook/vue'
 
 import App from '../../01-globals/app/App.vue'
-import AlpacaTabs from './Tabs.vue'
+import AlpacaTabs from './Tab.vue'
+import AlpacaTabsContainer from './TabsContainer.vue'
+import AlpacaTabContent from './TabContent.vue'
 
 import tabs from './mocks/tabs.json'
 
 storiesOf('Elements/Tabs', module).add('Default', () => ({
-  components: { App, AlpacaTabs },
+  components: { App, AlpacaTabs, AlpacaTabsContainer, AlpacaTabContent },
   data: () => ({
-    tabs
+    tabs,
+    activeTab: tabs.items[0].tabId
   }),
   template: `
     <app>
-    
-      <alpaca-tabs 
-        v-for="(tab, i) in tabs.items"
-        :index="i"
-        :key="tab.tabId"
-        :tab="tab"
-        :tabs="tabs.items"
-      >
-        {{ tab.content }}
-      </alpaca-tabs>
+      <alpaca-tabs-container>
+        <template
+          v-for="tab in tabs.items"
+        >
+          <alpaca-tabs 
+            :key="tab.tabId"
+            :tab-id="tab.tabId"
+            :title="tab.title"
+            :active-tab="activeTab"
+            @click="setActiveTab(tab.tabId)"
+          />
+          <alpaca-tab-content
+            :tab-id="tab.tabId"
+            :title-id="tab.titleId"
+            :active-tab="activeTab"
+          >
+            {{ tab.content }}
+          </alpaca-tab-content>
+        </template>
+      </alpaca-tabs-container>
     </app>
-  `
+  `,
+  methods: {
+    setActiveTab(tab) {
+      this.activeTab = this.tabs.items
+        .filter(el => el.tabId === tab)
+        .map(el => el.tabId)[0]
+    }
+  }
 }))
 .add('With icon', () => ({
-  components: { App, AlpacaTabs },
+  components: { App, AlpacaTabs, AlpacaTabsContainer, AlpacaTabContent },
   data: () => ({
-    tabs
+    tabs,
+    activeTab: tabs.items[0].tabId
   }),
   template: `
     <app>
-      <alpaca-tabs 
-        :tabs="tabs.items"
-        :icon="tabs.icon.iconId"
-      />
+      <alpaca-tabs-container>
+        <template
+          v-for="tab in tabs.items"
+        >
+          <alpaca-tabs 
+            :key="tab.tabId"
+            :tab-id="tab.tabId"
+            :title="tab.title"
+            :active-tab="activeTab"
+            :icon="tabs.icon.iconId"
+            @click="setActiveTab(tab.tabId)"
+          />
+          <alpaca-tab-content
+            :tab-id="tab.tabId"
+            :title-id="tab.titleId"
+            :active-tab="activeTab"
+          >
+            {{ tab.content }}
+          </alpaca-tab-content>
+        </template>
+      </alpaca-tabs-container>
     </app>
-  `
+  `,
+  methods: {
+    setActiveTab(tab) {
+      this.activeTab = this.tabs.items
+        .filter(el => el.tabId === tab)
+        .map(el => el.tabId)[0]
+    }
+  }
 }))
