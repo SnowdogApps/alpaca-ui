78661c51929a7082476fa92f95c87314f270a60a
Joanna Szczęch <joanna.szczech@snow.dog>
2019-02-02 21:47:24 +0000

    #49812 - Migrate information

---
+++ components/02-elements/information/Information.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="information">
+    <alpaca-heading
+      :level="5"
+      class="information__title"
+    >
+      {{ title }}
+    </alpaca-heading>
+    <alpaca-paragraph class="information__text">
+      {{ text }}
+    </alpaca-paragraph>
+  </div>
+</template>
+
+<script>
+  import AlpacaHeading from '../../01-globals/heading/Heading'
+  import AlpacaParagraph from '../../01-globals/paragraph/Paragraph'
+
+  export default {
+    components: {
+      AlpacaHeading,
+      AlpacaParagraph
+    },
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  $information-margin: 15px !default;
+
+  .information {
+    margin-bottom: $information-margin * 2;
+    &__title {
+      margin-bottom: $information-margin;
+    }
+    &__text {
+      margin-bottom: 0;
+    }
+  }
+</style>
