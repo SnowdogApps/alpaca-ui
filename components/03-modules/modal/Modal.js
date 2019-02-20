e1c1367af01ced1c3c60b0f105b16f887874fb33
Paweł Szafrański <pawel.szafranski@snow.dog>
2019-02-20 15:58:59 +0100

    #49696 - added modal component

---
+++ components/03-modules/modal/Modal.js
@@ -0,0 +1,79 @@
+import AlpacaIcon from '../../01-globals/icon/Icon.vue'
+
+export default {
+  components: {
+    AlpacaIcon
+  },
+  data() {
+    return {
+      visibility: false,
+      focused: null,
+      ariaHidden: 'true'
+    }
+  },
+  props: {
+    closeOnEsc: {
+      type: Boolean,
+      default: true
+    },
+    closeOnBackgroundClick: {
+      type: Boolean,
+      default: true
+    },
+    maxWidth: {
+      type: Number,
+      default: null
+    },
+    ariaLabelledby: {
+      type: String,
+      default: null
+    },
+    ariaDescribedby: {
+      type: String,
+      default: null
+    },
+    closeButton: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    show() {
+      // TODO: focustrap
+      this.ariaHidden = 'false'
+      this.focused = document.activeElement
+      this.visibility = !this.visibility
+      this.$nextTick(() => this.$refs.modal.focus())
+    },
+    hide() {
+      this.ariaHidden = 'true'
+      this.visibility = !this.visibility
+      this.$nextTick(() => this.focused.focus())
+    },
+    handleBackgroundClick() {
+      if (this.closeOnBackgroundClick) {
+        this.hide()
+      }
+    },
+    handleEscapeKeyUp (event) {
+      if (event.which === 27 && this.visibility) {
+        this.hide()
+      }
+    }
+  },
+  computed: {
+    getMaxWidth () {
+      return this.maxWidth !== null ? `max-width: ${this.maxWidth}px;`: null
+    }
+  },
+  beforeMount () {
+    if (this.closeOnEsc) {
+      window.addEventListener('keydown', this.handleEscapeKeyUp)
+    }
+  },
+  destroyed () { 
+    if (this.closeOnEsc) {
+      window.removeEventListener('keydown', this.handleEscapeKeyUp)
+    }
+  }
+}
