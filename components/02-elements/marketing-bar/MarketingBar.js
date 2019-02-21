1764ddd034ccb939f89236822a5ee25399019429
Joanna Szczęch <joanna.szczech@snow.dog>
2019-02-22 00:59:59 +0100

    #50553 - MarketingBar - move to separate files

---
+++ components/02-elements/marketing-bar/MarketingBar.js
@@ -0,0 +1,30 @@
+import AlpacaButton from '../button/Button.vue'
+
+export default {
+  components: {
+    AlpacaButton
+  },
+  props: {
+    dataType: {
+      type: String,
+      default: 'marketing-bar'
+    }
+  },
+  data() {
+    return {
+      isBarVisible: true
+    }
+  },
+  mounted() {
+    // Display message if it wasn't closed before
+    if (localStorage.getItem(this.dataType) !== 'closed') {
+      this.isBarVisible = true
+    }
+  },
+  methods: {
+    onButtonClose() {
+      localStorage.setItem(this.dataType, 'closed')
+      this.isBarVisible = false
+    }
+  }
+}
