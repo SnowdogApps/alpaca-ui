2cab8445231a639b0b4f203ea8f1ee1a21c3fc8b
Joanna Szczęch <joanna.szczech@snow.dog>
2019-02-22 00:11:18 +0100

    #50553 - Banner - move to separate files

--- components/02-elements/banner/Banner.vue
+++ components/02-elements/banner/Banner.vue
@@ -1,67 +1,11 @@
-<template>
-  <a
-    :href="link"
-    class="banner"
-    :aria-label="ariaLabel"
-  >
-    <picture class="image">
-      <source
-        v-for="{ key, source } in getImageWithKey"
-        :key="key"
-        :srcset="source.src"
-        :media="source.mediaQuery"
-      >
-      <img
-        class="lazyload"
-        :src="image.defaultSrc"
-        :alt="image.alt"
-      >
-    </picture>
-  </a>
-</template>
+<template src="./Banner.html" />
 
 <script>
-  import uniqueId from 'lodash.uniqueid'
+  import instance from "./Banner.js"
 
   export default {
-    props: {
-      link: {
-        type: String,
-        required: true
-      },
-      image: {
-        type: Object,
-        required: true
-      },
-      ariaLabel: {
-        type: String,
-        default: null
-      }
-    },
-    computed: {
-      getImageWithKey() {
-        return this.image.sources.map(source => ({ key: uniqueId("image"), source }))
-      }
-    },
+    ...instance
   }
 </script>
 
-<style lang="scss">
-  .banner {
-    display: block;
-    position: relative;
-    width: 100%;
-
-    &:hover {
-      text-decoration: none;
-    }
-
-    &:focus {
-      outline: none;
-    }
-
-    &__image {
-      width: 100%;
-    }
-  }
-</style>
+<style lang="scss" src="./Banner.scss"/>
