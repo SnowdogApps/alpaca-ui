f6c357a24ecd8d56d7a2c42aea329b3ad0e3470a
Joanna Szczęch <joanna.szczech@snow.dog>
2019-02-22 11:26:48 +0100

    #50553 - Gallery - move to separate files

---
+++ components/03-modules/gallery/Gallery.js
@@ -0,0 +1,55 @@
+import uniqueId from 'lodash.uniqueid'
+
+import AlpacaBadge from '../../02-elements/badge/Badge.vue'
+import AlpacaImage from '../../02-elements/image/Image.vue'
+import AlpacaIcon from '../../01-globals/icon/Icon.vue'
+
+export default {
+  components: {
+    AlpacaBadge,
+    AlpacaImage,
+    AlpacaIcon
+  },
+  props: {
+    images: {
+      type: Array,
+      required: true
+    },
+    horizontal: {
+      type: Boolean,
+      default: null
+    },
+    productLabel: {
+      type: Object,
+      default: null
+    },
+    mainImage: {
+      type: Number,
+      default: 1
+    }
+  },
+  data(){
+    return {
+      currentImage: this.mainImage === 0
+        ? this.mainImage
+        : this.mainImage >= this.images.length
+          ? this.images.length - 1
+          : this.mainImage -1
+    }
+  },
+  computed: {
+    getThumbsWithKey() {
+      return this.images.map(image => ({ key: uniqueId("image"), image }))
+    },
+    selectedImage() {
+      return this.images[this.currentImage]
+    }
+  },
+  methods: {
+    setCurrentImage (val) {
+      if(this.images[val]){
+        this.currentImage = val
+      }
+    }
+  }
+}
