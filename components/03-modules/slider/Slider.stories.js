4b1461767cdfc990bdaa21c63867401cffb36d2a
talalus <talal.suhail@snow.dog>
2019-02-27 01:13:22 +0200

    #49905: add alpaca slider as wrapper for vue-carousel

---
+++ components/03-modules/slider/Slider.stories.js
@@ -0,0 +1,57 @@
+import { storiesOf } from '@storybook/vue'
+import data from './mocks/data'
+
+import App from '../../01-globals/app/App.vue'
+import AlpacaSlider from './Slider.vue'
+import AlpacaIcon from '../../01-globals/icon/Icon.vue'
+import AlpacaImage from '../../02-elements/image/Image'
+import { Slide } from 'vue-carousel'
+
+storiesOf('Modules/Slider', module)
+  .add('Default', () => ({
+    components: { App, AlpacaSlider, Slide, AlpacaImage, AlpacaIcon },
+    data: () => {
+      return {
+        ...data,
+      }
+    },
+    template: `
+      <app>
+        <alpaca-slider>
+          <template>
+            <slide class="slider__item" :key="index" v-for="(slide, index) in slides">
+              <div class="lazyload-wrapper">
+                <alpaca-image
+                  :src="slide.thumbnail"
+                  :alt="slide.alt"
+                />
+              </div>
+            </slide>
+          </template>
+        </alpaca-slider>
+      </app>
+    `
+  })).add('With dots', () => ({
+  components: { App, AlpacaSlider, Slide, AlpacaImage, AlpacaIcon },
+  data: () => {
+    return {
+      ...data,
+    }
+  },
+  template: `
+      <app>
+        <alpaca-slider :paginationEnabled="true">
+          <template>
+            <slide class="slider__item" :key="index" v-for="(slide, index) in slides">
+              <div class="lazyload-wrapper">
+                <alpaca-image
+                  :src="slide.thumbnail"
+                  :alt="slide.alt"
+                />
+              </div>
+            </slide>
+          </template>
+        </alpaca-slider>
+      </app>
+    `
+}))
