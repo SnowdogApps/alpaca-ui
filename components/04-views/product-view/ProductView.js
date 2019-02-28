9d368d2bfd6f60c1ce6a715238435145f3b40a82
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2019-02-28 13:41:43 +0100

    #49901 - Add name, sku and price to product-view

--- components/04-views/product-view/ProductView.js
+++ components/04-views/product-view/ProductView.js
@@ -1,20 +1,26 @@
 import breadcrumbs from '../../02-elements/breadcrumbs/mocks/breadcrumbs.json'
 import gallery from '../../03-modules/gallery/mocks/gallery.json';
+import product from './mocks/product.json';
 
 import AlpacaContainer from '../../01-globals/container/Container.vue'
+import AlpacaHeading from '../../01-globals/heading/Heading.vue'
 import AlpacaBreadcrumbs from '../../02-elements/breadcrumbs/Breadcrumbs.vue'
+import AlpacaPrice from '../../02-elements/price/Price.vue'
 import AlpacaGallery from '../../03-modules/gallery/Gallery.vue'
 
 export default {
   components: {
     AlpacaContainer,
+    AlpacaHeading,
     AlpacaBreadcrumbs,
+    AlpacaPrice,
     AlpacaGallery
   },
   data() {
     return {
       breadcrumbs,
-      gallery
+      gallery,
+      product
     }
   }
 }
