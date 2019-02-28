bef916655deec9081506bf46fd9454f835e48773
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2019-02-28 12:40:31 +0100

    #49901 - Add breadcrumbs and gallery to product view

--- components/04-views/product-view/ProductView.js
+++ components/04-views/product-view/ProductView.js
@@ -1,7 +1,20 @@
+import breadcrumbs from '../../02-elements/breadcrumbs/mocks/breadcrumbs.json'
+import gallery from '../../03-modules/gallery/mocks/gallery.json';
+
 import AlpacaContainer from '../../01-globals/container/Container.vue'
+import AlpacaBreadcrumbs from '../../02-elements/breadcrumbs/Breadcrumbs.vue'
+import AlpacaGallery from '../../03-modules/gallery/Gallery.vue'
 
 export default {
   components: {
-    AlpacaContainer
+    AlpacaContainer,
+    AlpacaBreadcrumbs,
+    AlpacaGallery
+  },
+  data() {
+    return {
+      breadcrumbs,
+      gallery
+    }
   }
 }
