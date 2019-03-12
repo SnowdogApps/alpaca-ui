ef8c63d6a4f3d85bf5bf529716fe41bd86ec16e3
Paweł Szafrański <pawel.szafranski@snow.dog>
2019-03-12 13:30:09 +0100

    Divide component to multiple files

---
+++ components/03-modules/login/Login.js
@@ -0,0 +1,62 @@
+import AlpacaFieldset from '../../02-elements/form/fieldset/Fieldset.vue'
+import AlpacaButton from '../../02-elements/button/Button.vue'
+import AlpacaInput from '../../02-elements/form/input/Input.vue'
+
+export default {
+  components: {
+    AlpacaFieldset,
+    AlpacaButton,
+    AlpacaInput
+  },
+  props: {
+    legend: {
+      type: String,
+      required: true
+    },
+    emailLabel: {
+      type: String,
+      default: 'Email'
+    },
+    emailPlaceholder: {
+      type: String,
+      default: 'Email'
+    },
+    passwordLabel: {
+      type: String,
+      default: 'Password'
+    },
+    passwordPlaceholder: {
+      type: String,
+      default: 'Password'
+    },
+    goToForgetPasswordButton: {
+      type: String,
+      required: true
+    },
+    goToRegisterButton: {
+      type: String,
+      required: true
+    },
+    submitButton: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    login() {
+      this.$emit('login', { email: this.email, password: this.password })
+    },
+    goToRegister() {
+      this.$emit('goToRegister')
+    },
+    goToForgotPassword() {
+      this.$emit('goToForgotPassword')
+    }
+  }
+}
