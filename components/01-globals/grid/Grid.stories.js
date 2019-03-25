f0ab3f5a4fa176fcd6554c167c2e4aa89994ed18
Bartek Igielski <igloczek@gmail.com>
2019-03-25 21:58:33 +0100

    Grid layout config moved from mocks to props

--- components/01-globals/grid/Grid.stories.js
+++ components/01-globals/grid/Grid.stories.js
@@ -5,189 +5,258 @@ import AlpacaGrid from './Grid.vue'
 
 // Preview styles
 import './preview/styles.css'
-import autoWidth from './mocks/autoWidth'
-import reordering from './mocks/reordering'
-import distribution from './mocks/distribution'
-import alignmentStart from './mocks/alignmentStart'
-import alignmentMiddle from './mocks/alignmentMiddle'
-import reversing from './mocks/reversing'
 
 storiesOf('Global/Grid', module)
   .add('Default', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      grid
-    }),
     template: `
       <app>
         <alpaca-grid
-          class="docs-only-styles"
-          :grids="grid"/>
+          :grids="[
+            [
+              'col-xs-12 col-sm-3 col-md-2 col-lg-1',
+              'col-xs-6 col-sm-6 col-md-8 col-lg-10',
+              'col-xs-6 col-sm-3 col-md-2 col-lg-1'
+            ],
+            [
+              'col-xs-12 col-sm-3 col-md-2 col-lg-1',
+              'col-xs-12 col-sm-9 col-md-10 col-lg-11'
+            ],
+            [
+              'col-xs-10 col-sm-6 col-md-8 col-lg-10',
+              'col-xs-2 col-sm-6 col-md-4 col-lg-2'
+            ]
+          ]"
+        />
       </app>
     `
   }))
   .add('Fluid', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      fluid
-    }),
     template: `
       <app>
         <alpaca-grid
-          class="docs-only-styles"
-          :grids="fluid"/>
+          :grids="[
+            [
+              'col-xs-12'
+            ],
+            [
+              'col-xs-1',
+              'col-xs-11'
+            ],
+            [
+              'col-xs-2',
+              'col-xs-10'
+            ],
+            [
+              'col-xs-3',
+              'col-xs-9'
+            ],
+            [
+              'col-xs-4',
+              'col-xs-8'
+            ],
+            [
+              'col-xs-5',
+              'col-xs-7'
+            ],
+            [
+              'col-xs-6',
+              'col-xs-6'
+            ]
+          ]"
+        />
       </app>
     `
   }))
   .add('Auto width', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      autoWidth
-    }),
     template: `
       <app>
         <alpaca-grid
-          class="docs-only-styles"
-          :grids="autoWidth"/>
+          :grids="[
+            [
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs'
+            ],
+            [
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs'
+            ]
+          ]"
+        />
       </app>
     `
   }))
   .add('Reordering', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      reordering
-    }),
     template: `
       <app>
         <alpaca-grid
-          class="docs-only-styles"
-          :grids="reordering"/>
+          :grids="[
+            [
+              'col-xs-2',
+              'col-xs-2',
+              'col-xs-2',
+              'col-xs-2',
+              'col-xs-2',
+              'col-xs-2 first-xs'
+            ],
+            [
+              'col-xs-2',
+              'col-xs-2',
+              'col-xs-2 last-xs',
+              'col-xs-2',
+              'col-xs-2',
+              'col-xs-2'
+            ]
+          ]"
+        />
       </app>
     `
   }))
   .add('Distribution around', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      distribution
-    }),
     template: `
       <app>
         <alpaca-grid
-          class="docs-only-styles"
           row-class="around-xs"
-          :grids="distribution"/>
+          :grids="[
+            'col-xs-2',
+            'col-xs-2',
+            'col-xs-2'
+          ]"
+        />
       </app>
     `
   }))
   .add('Distribution between', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      distribution
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="between-xs"
-          class="docs-only-styles"
-          :grids="distribution"/>
+          :grids="[
+            'col-xs-2',
+            'col-xs-2',
+            'col-xs-2'
+          ]"
+        />
       </app>
     `
   }))
   .add('Alignment start', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      alignmentStart
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="start-xs"
-          class="docs-only-styles"
-          :grids="alignmentStart"/>
+          :grids="[
+            ['col-xs-6']
+          ]"
+        />
       </app>
     `
   }))
   .add('Alignment center', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      alignmentStart
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="center-xs"
-          class="docs-only-styles"
-          :grids="alignmentStart"/>
+          :grids="[
+            ['col-xs-6']
+          ]"
+        />
       </app>
     `
   }))
   .add('Alignment end', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      alignmentStart
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="end-xs"
-          class="docs-only-styles"
-          :grids="alignmentStart"/>
+          :grids="[
+            ['col-xs-6']
+          ]"
+        />
       </app>
     `
   }))
   .add('Alignment top', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      alignmentMiddle
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="top-xs"
-          class="docs-only-styles"
-          :grids="alignmentMiddle"/>
+          :grids="[
+            [
+              'col-xs col-large',
+              'col-xs'
+            ]
+          ]"
+        />
       </app>
     `
   }))
   .add('Alignment middle', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      alignmentMiddle
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="middle-xs"
-          class="docs-only-styles"
-          :grids="alignmentMiddle"/>
+          :grids="[
+            [
+              'col-xs col-large',
+              'col-xs'
+            ]
+          ]"
+        />
       </app>
     `
   }))
   .add('Alignment bottom', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      alignmentMiddle
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="bottom-xs"
-          class="docs-only-styles"
-          :grids="alignmentMiddle"/>
+          :grids="[
+            [
+              'col-xs col-large',
+              'col-xs'
+            ]
+          ]"
+        />
       </app>
     `
   }))
   .add('Reversing', () => ({
     components: { App, AlpacaGrid },
-    data: () => ({
-      reversing
-    }),
     template: `
       <app>
         <alpaca-grid
           row-class="reverse"
-          class="docs-only-styles"
-          :grids="reversing"/>
+          :grids="[
+            [
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs',
+              'col-xs'
+            ]
+          ]"
+        />
       </app>
     `
   }))
