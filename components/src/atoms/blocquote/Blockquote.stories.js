060db5a8c37c0915aebdf2ae321fae94a57a3028
Joanna Szczęch <joanna.szczech@snow.dog>
2019-03-27 17:11:40 +0100

    #51852 - Move blockquote component to atoms

--- components/src/atoms/blocquote/Blockquote.stories.js
+++ components/src/atoms/blocquote/Blockquote.stories.js
@@ -1,21 +1,27 @@
 import { storiesOf } from '@storybook/vue'
 
-import AlpacaBlockquote from './Blockquote.vue'
+import ABlockquote from './Blockquote.vue'
 
-storiesOf('Global/Blockquote', module)
+const info = `
+  This component does not have any modifiers.
+`
+
+storiesOf('Atoms/Blockquote', module)
   .add('Default', () => ({
-    components: { AlpacaBlockquote },
+    components: { ABlockquote },
     template: `
-        <alpaca-blockquote>
+        <a-blockquote>
           Qui quis labore ad laborum laborum ipsum veniam sunt. Laboris tempor id ut commodo aute incididunt consequat esse aute irure. Et irure elit veniam proident non do commodo labore cillum irure laborum ullamco nostrud.
-        </alpaca-blockquote>
+        </a-blockquote>
     `
-  }))
+  }),
+    { info }
+  )
   .add('Custom tag', () => ({
-    components: { AlpacaBlockquote },
+    components: { ABlockquote },
     template: `
-        <alpaca-blockquote tag="span">
+        <a-blockquote tag="span">
           Ipsum consequat do eiusmod irure dolor labore mollit commodo. Culpa eu Lorem fugiat incididunt velit incididunt reprehenderit quis do anim culpa. Proident aute incididunt non anim. Est aute laborum aliquip duis. Deserunt velit velit veniam reprehenderit laboris cupidatat cupidatat elit. Ad duis est tempor consequat incididunt.
-        </alpaca-blockquote>
+        </a-blockquote>
     `
   }))
