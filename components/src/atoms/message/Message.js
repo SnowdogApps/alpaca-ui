2659f86baf387ab258a63f00dba2492ddf4a3225
Joanna Szczęch <joanna.szczech@snow.dog>
2019-03-28 18:55:44 +0100

    #51852 - Move message component to atoms

---
+++ components/src/atoms/message/Message.js
@@ -0,0 +1,11 @@
+import AIcon from '../icon/Icon.vue'
+
+export default {
+  components: { AIcon },
+  props: {
+    icon: {
+      type: String,
+      default: null
+    }
+  }
+}
---
+++ components/src/atoms/message/Message.stories.js
@@ -0,0 +1,53 @@
+import { storiesOf } from '@storybook/vue'
+
+import AMessage from './Message.vue'
+
+const info = `
+  ------
+  #### Available BEM modifiers for \`.message\` class:
+  - \`--success\`
+  - \`--error\`
+  - \`--warning\`
+  ------
+`
+
+storiesOf('Atoms/Message', module)
+  .add('Default', () => ({
+    components: { AMessage },
+    template: `
+      <a-message 
+        class="message--success"
+        icon="check"
+      >
+        You added Jacket to your shopping cart.
+      </a-message>
+    `
+    }),
+    { info }
+  )
+  .add('Warning', () => ({
+    components: { AMessage },
+    template: `
+      <a-message
+        class="message--warning"
+        icon="warning"
+      >
+        You added Jacket to your shopping cart.
+      </a-message>
+    `
+    }),
+    { info }
+  )
+  .add('Error', () => ({
+    components: { AMessage },
+    template: `
+      <a-message
+        class="message--error"
+        icon="error"
+      >
+        You added Jacket to your shopping cart.
+      </a-message>
+    `
+    }),
+    { info }
+  )
