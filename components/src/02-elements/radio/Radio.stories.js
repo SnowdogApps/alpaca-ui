4d6f81bb7e2994615043d3d92a5ec96be9903cda
Robert Wozniak <30414211+robwozniak@users.noreply.github.com>
2019-03-29 10:33:06 +0100

    #143 - Update radio component
    Accept content via slots, allow deselect, update markup and styles.

--- components/src/02-elements/radio/Radio.stories.js
+++ components/src/02-elements/radio/Radio.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/vue'
 import options from "./mocks/radio"
 
 import AlpacaRadio from './Radio.vue'
+import AlpacaIcon from '../../01-globals/icon/Icon.vue'
 
 storiesOf('Elements/Form/Radio', module)
   .add('Default', () => ({
@@ -13,11 +14,58 @@ storiesOf('Elements/Form/Radio', module)
       }
     },
     template: `
-        <alpaca-radio
-          :legendId="options.legendId"
-          :legend="options.legend"
-          :options="options.options"
-          :name="options.name"
-        />
+      <alpaca-radio
+        :options="options.options"
+        :name="options.name"
+      />
+    `
+  }))
+  .add('Allow deselect', () => ({
+    components: { AlpacaRadio },
+    data() {
+      return {
+        options
+      }
+    },
+    template: `
+      <alpaca-radio
+        :options="options.options"
+        :name="options.name"
+        wrapperTag="fieldset"
+        style="padding: 20px 10px 10px 10px;"
+        allowDeselect
+      >
+        <template v-slot:wrapper>
+          <legend>Choose an option</legend>
+        </template>
+      </alpaca-radio>
+    `
+  }))
+  .add('With check icon', () => ({
+    components: { AlpacaRadio, AlpacaIcon },
+    data() {
+      return {
+        options
+      }
+    },
+    template: `
+      <alpaca-radio
+        :options="options.options"
+        :name="options.name"
+        wrapperTag="fieldset"
+        style="padding: 20px 10px 10px 10px;"
+        allowDeselect
+      >
+        <template v-slot:wrapper>
+          <legend>Choose an option</legend>
+        </template>
+        <template v-slot:option="data">
+          <alpaca-icon
+            icon="checked"
+            style="margin-right: 8px;"
+          />
+          {{ data.option.label }}
+        </template>
+      </alpaca-radio>
     `
   }))
