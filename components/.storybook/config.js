70a596145a346807bf7fb2fee03a2faac70eec43
Paweł Szafrański <pawel.szafranski@snow.dog>
2019-04-03 00:25:24 +0200

    Added a11y addon with basic config

--- components/.storybook/config.js
+++ components/.storybook/config.js
@@ -1,14 +1,15 @@
 // Storybook
 import { configure, addDecorator, addParameters } from '@storybook/vue'
 
-// Info Addon
+// Addons
 import { withInfo } from 'storybook-addon-vue-info'
+import { withA11y } from '@storybook/addon-a11y'
 
 // Wrap every story in Alpaca App component
 import AApp from '../src/01-globals/app/App.vue'
 
 addDecorator(withInfo)
-
+addDecorator(withA11y)
 addDecorator(() => ({
   components: {
     AApp
