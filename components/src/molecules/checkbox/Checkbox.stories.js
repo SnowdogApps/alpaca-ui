6edc8b44c6a6ef0f4a43bfcc13605b415d51ec85
Paweł Szafrański <pawel.szafranski@snow.dog>
2019-04-04 01:06:47 +0200

    #52000 - formatting

--- components/src/molecules/checkbox/Checkbox.stories.js
+++ components/src/molecules/checkbox/Checkbox.stories.js
@@ -8,103 +8,115 @@ import './preview/styles.css'
 
 storiesOf('Molecules/Checkbox', module)
   .addParameters({ info: true })
-  .add('Default', () => ({
-    components: { ACheckbox },
-    data: () => {
-      return {
-        status: true
+  .add(
+    'Default',
+    () => ({
+      components: { ACheckbox },
+      data: () => {
+        return {
+          status: true
+        }
+      },
+      template: `
+        <div>
+          <a-checkbox
+            id="checkbox-default"
+            name="checkbox-default"
+            v-model="status"
+            @input="onChange"
+          >
+            Checkbox field
+          </a-checkbox>
+          Selected: {{ status }}
+        </div>
+      `,
+      methods: {
+        onChange: action('Option changed')
       }
-    },
-    template: `
-      <div>
-        <a-checkbox
-          id="checkbox-default"
-          name="checkbox-default"
-          v-model="status"
-          @input="onChange"
-        >
-          Checkbox field
-        </a-checkbox>
-        Selected: {{ status }}
-      </div>
-    `,
-    methods: {
-      onChange: action('Option changed')
-    }
-  }))
-  .add('Custom Values', () => ({
-    components: { ACheckbox },
-    data: () => {
-      return {
-        status: 'not_accepted'
+    })
+  )
+  .add(
+    'Custom Values',
+    () => ({
+      components: { ACheckbox },
+      data: () => {
+        return {
+          status: 'not_accepted'
+        }
+      },
+      template: `
+        <div>
+          <a-checkbox
+            id="checkbox-values"
+            name="checkbox-values"
+            v-model="status"
+            value="accepted"
+            unchecked-value="not_accepted"
+            @input="onChange"
+          >
+            Checkbox field
+          </a-checkbox>
+          Selected: {{ status }}
+        </div>
+      `,
+      methods: {
+        onChange: action('Option changed')
       }
-    },
-    template: `
-      <div>
-        <a-checkbox
-          id="checkbox-default"
-          name="checkbox-default"
-          v-model="status"
-          value="accepted"
-          unchecked-value="not_accepted"
-          @input="onChange"
-        >
-          Checkbox field
-        </a-checkbox>
-        Selected: {{ status }}
-      </div>
-    `,
-    methods: {
-      onChange: action('Option changed')
-    }
-  }))
-  .add('Long label', () => ({
-    data: () => {
-      return {
-        selected: null
-      }
-    },
-    components: { ACheckbox },
-    template: `
-      <div>
-        <a-checkbox
-          id="checkbox-long-label"
-          name="checkbox-long-label-name"
-          value="checkbox-long-label-value"
-          v-model="selected"
-        >
-          I hereby agree for processing my personal data, included in my job offer, for the purpose of recruitment (as defined in the Act of August 29, 1997 on the Protection of Personal Data (Journal of Laws No. 133, item 883).
-        </a-checkbox>
-        Selected: {{ selected }}
-      </div>
-    `
-  }))
-  .add('With slot', () => ({
-    components: { ACheckbox },
-    data: () => {
-      return {
-        selected: null
-      }
-    },
-    template: `
-      <div>
-        <a-checkbox
-          id="checkbox-slots"
-          name="checkbox-slots"
-          value="checkbox-slots"
-          input-class="checkbox__field-custom"
-          v-model="selected"
-        >
-          <template #label>
-            <label
-              for="checkbox-slots"
-              class="checkbox__label-custom"
-            >
-              I hereby agree for processing my personal data.
-            </label>
-          </template>
-        </a-checkbox>
-        Selected: {{ selected }}
-      </div>
-    `
-  }))
+    })
+  )
+  .add(
+    'Long label',
+    () => ({
+      data: () => {
+        return {
+          selected: null
+        }
+      },
+      components: { ACheckbox },
+      template: `
+        <div>
+          <a-checkbox
+            id="checkbox-long-label"
+            name="checkbox-long-label-name"
+            value="checkbox-long-label-value"
+            v-model="selected"
+          >
+            I hereby agree for processing my personal data, included in my job offer, for the purpose of recruitment (as defined in the Act of August 29, 1997 on the Protection of Personal Data (Journal of Laws No. 133, item 883).
+          </a-checkbox>
+          Selected: {{ selected }}
+        </div>
+      `
+    })
+  )
+  .add(
+    'With slot',
+    () => ({
+      components: { ACheckbox },
+      data: () => {
+        return {
+          selected: null
+        }
+      },
+      template: `
+        <div>
+          <a-checkbox
+            id="checkbox-slots"
+            name="checkbox-slots"
+            value="checkbox-slots"
+            input-class="checkbox__field-custom"
+            v-model="selected"
+          >
+            <template #label>
+              <label
+                for="checkbox-slots"
+                class="checkbox__label-custom"
+              >
+                I hereby agree for processing my personal data.
+              </label>
+            </template>
+          </a-checkbox>
+          Selected: {{ selected }}
+        </div>
+      `
+    })
+  )
