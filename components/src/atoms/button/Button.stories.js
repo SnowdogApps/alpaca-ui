972287303c8c99e9cd417ebaf8cfeb3ac2a154e1
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-04 16:16:34 +0200

    #52097 - Formatting

--- components/src/atoms/button/Button.stories.js
+++ components/src/atoms/button/Button.stories.js
@@ -16,79 +16,97 @@ const info = `
 
 storiesOf('Atoms/Button', module)
   .addParameters({ info })
-  .add('Default', () => ({
-    components: { AButton },
-    template: `
-      <a-button type="button">
-        Default button
-      </a-button>
+  .add(
+    'Default',
+    () => ({
+      components: { AButton },
+      template: `
+        <a-button type="button">
+          Default button
+        </a-button>
     `
-  }))
-  .add('Secondary', () => ({
-    components: { AButton },
-    template: `
-      <a-button
-        class="button--secondary"
-        type="button"
-      >
-        Secondary button
-      </a-button>
+    })
+  )
+  .add(
+    'Secondary',
+    () => ({
+      components: { AButton },
+      template: `
+        <a-button
+          class="button--secondary"
+          type="button"
+        >
+          Secondary button
+        </a-button>
     `
-  }))
-  .add('Blank', () => ({
-    components: { AButton },
-    template: `
-      <a-button
-        class="button--blank"
-        type="button"
-      >
-        Blank button
-      </a-button>
+    })
+  )
+  .add(
+    'Blank',
+    () => ({
+      components: { AButton },
+      template: `
+        <a-button
+          class="button--blank"
+          type="button"
+        >
+          Blank button
+        </a-button>
     `
-  }))
-  .add('Fluid', () => ({
-    components: { AButton },
-    template: `
-      <a-button
-        class="button--fluid"
-        type="button"
-      >
-        Fluid button
-      </a-button>
+    })
+  )
+  .add(
+    'Fluid',
+    () => ({
+      components: { AButton },
+      template: `
+        <a-button
+          class="button--fluid"
+          type="button"
+        >
+          Fluid button
+        </a-button>
     `
-  }))
-  .add('Icon', () => ({
-    components: { AButton, AIcon },
-    template: `
-      <a-button
-        class="button--icon"
-        type="button"
-      >
-        <template>
-          <a-icon
-            icon="search"
-            class="button__icon"
-            title="iconTitle"
-          />
-        </template>
-      </a-button>
+    })
+  )
+  .add(
+    'Icon',
+    () => ({
+      components: { AButton, AIcon },
+      template: `
+        <a-button
+          class="button--icon"
+          type="button"
+        >
+          <template>
+            <a-icon
+              icon="search"
+              class="button__icon"
+              title="iconTitle"
+            />
+          </template>
+        </a-button>
     `
-  }))
-  .add('Icon and text', () => ({
-    components: { AButton, AIcon },
-    template: `
-      <a-button
-        class="button--icon-custom"
-        type="button"
-      >
-        <template>
-          Search
-          <a-icon
-            icon="search"
-            class="button__icon-custom"
-            title="iconTitle"
-          />
-        </template>
-      </a-button>
+    })
+  )
+  .add(
+    'Icon and text',
+    () => ({
+      components: { AButton, AIcon },
+      template: `
+        <a-button 
+          class="button--icon-custom"
+          type="button"
+        >
+          <template>
+            Search
+            <a-icon
+              icon="search"
+              class="button__icon-custom"
+              title="iconTitle"
+            />
+          </template>
+        </a-button>
     `
-  }))
+    })
+  )
