59097853290d8c03a97bd96e8df86609199b269f
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-04 20:09:48 +0200

    #52129 - Formatting

--- components/src/atoms/price/Price.stories.js
+++ components/src/atoms/price/Price.stories.js
@@ -15,53 +15,72 @@ const defaultData = {
 
 storiesOf('Atoms/Price', module)
   .addParameters({ info: true })
-  .add('Default', () => ({
-    ...defaultData,
-    template: `
-      <a-price>
-        <span>{{ price }}</span>
-      </a-price>
-    `
-  }))
-  .add('Special Price', () => ({
-    ...defaultData,
-    template: `
-      <a-price 
-        :special-price="special"
-        aria-label-special="Special price:"
-      />
-    `
-  }))
-  .add('Old price', () => ({
-    ...defaultData,
-    template: `
-      <a-price 
-        :old-price="old"
-        aria-label-old="Old price:"
-      />
-    `
-  }))
-  .add('With special price', () => ({
-    ...defaultData,
-    template: `
-      <a-price
-        :old-price="old"
-        :special-price="special"
-        aria-label-special="Special price:"
-        aria-label-old="Old price:"
-      />
-    `
-  }))
-  .add('With slots', () => ({
-    ...defaultData,
-    template: `
-      <a-price>
-        <template #oldPrice>
-          <span :style="{color: '#73739c'}"> {{old}} </span>&nbsp;
-        </template> 
-        <template #specialPrice>
-          <span :style="{color: '#e32926'}"> {{special}} </span>
-        </template>
-      </a-price>
-    `
-  }))
+  .add(
+    'Default',
+    () => ({
+      ...defaultData,
+      template: `
+        <a-price>
+          <span>{{ price }}</span>
+        </a-price>
+      `
+    })
+  )
+  .add(
+    'Special Price',
+    () => ({
+      ...defaultData,
+      template: `
+        <a-price 
+          :special-price="special"
+          aria-label-special="Special price:"
+        />
+      `
+    })
+  )
+  .add(
+    'Old price',
+    () => ({
+      ...defaultData,
+      template: `
+        <a-price 
+          :old-price="old"
+          aria-label-old="Old price:"
+        />
+      `
+    })
+  )
+  .add(
+    'With special price',
+    () => ({
+      ...defaultData,
+      template: `
+        <a-price
+          :old-price="old"
+          :special-price="special"
+          aria-label-special="Special price:"
+          aria-label-old="Old price:"
+        />
+      `
+    })
+  )
+  .add(
+    'With slots',
+    () => ({
+      ...defaultData,
+      template: `
+        <a-price>
+          <template #oldPrice>
+            <span :style="{color: '#73739c'}"> 
+              {{old}} 
+            </span>&nbsp;
+          </template> 
+          <template #specialPrice>
+            <span :style="{color: '#e32926'}"> 
+              {{special}}
+            </span>
+          </template>
+        </a-price>
+      `
+   })
+  )
