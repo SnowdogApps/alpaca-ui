0230357af282ec5731c96a4af6ea8b0fe1281362
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-04 15:14:57 +0200

    #52173 - Add vue-info

--- components/src/molecules/logo/Logo.js
+++ components/src/molecules/logo/Logo.js
@@ -3,14 +3,23 @@ import AImage from '../../atoms/image/Image.vue'
 export default {
   components: { AImage },
   props: {
+    /**
+     * Redirect link
+     */
     to: {
       type: String,
       required: true
     },
+    /**
+     * Src for logo
+     */
     src: {
       type: String,
       default: null
     },
+    /**
+     * Alt for logo
+     */
     alt: {
       type: String,
       default: null
--- components/src/molecules/logo/Logo.stories.js
+++ components/src/molecules/logo/Logo.stories.js
@@ -5,29 +5,28 @@ import ALogo from './Logo.vue'
 
 storiesOf('Molecules/Logo', module)
   .addDecorator(StoryRouter())
+  .addParameters({ info: true })
   .add('Default', () => ({
     components: { ALogo },
     template: `
-        <a-logo
-          to="/"
-          src="/images/logo/alpaca.svg"
-          alt="Logo"
-        />
+      <a-logo
+        to="/"
+        src="/images/logo/alpaca.svg"
+        alt="Logo"
+      />
     `
   }))
   .add('With slots', () => ({
     components: { ALogo },
     template: `
-        <a-logo to="/">
-          <template #image>
-            <img 
-              src="/images/logo/alpaca.svg" 
-              :style="{
-                boxShadow: '0 4px 4px 0 rgba(51, 51, 51, 0.3)', 
-                padding: '5px'
-              }"
-            />
-          </template>
-        </a-logo>
+      <a-logo to="/">
+        <template #image>
+          <img 
+            src="/images/logo/alpaca.svg" 
+            alt="Logo"
+            :style="{width: '70%'}"
+          />
+        </template>
+      </a-logo>
     `
   }))
