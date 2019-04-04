e61dc9962f6c121ac0fad38fa51003d08f878536
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-04 18:34:54 +0200

    #52190 - Refactor description list

--- components/src/02-elements/description-list/DescriptionList.js
+++ components/src/02-elements/description-list/DescriptionList.js
@@ -1,16 +1,11 @@
 export default {
   props: {
+    /**
+     * Array with elements. All object should have keys: id, label and value.
+     */
     elements: {
       type: Array,
       required: true
-    },
-    labelClass: {
-      type: String,
-      default: null
-    },
-    valueClass: {
-      type: String,
-      default: null
     }
   }
 }
--- components/src/02-elements/description-list/DescriptionList.vue
+++ components/src/02-elements/description-list/DescriptionList.vue
@@ -1,11 +1,5 @@
 <template src="./DescriptionList.html" />
 
-<script>
-  import instance from "./DescriptionList.js"
-
-  export default {
-    ...instance
-  }
-</script>
+<script src="./DescriptionList.js"/>
 
 <style lang="scss" src="./DescriptionList.scss"/>
