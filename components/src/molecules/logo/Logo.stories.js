834f1dc04fe3dc5d2d8f115f42a3c23bd420e6ed
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-04 15:48:02 +0200

    #52173 - Formatting

--- components/src/molecules/logo/Logo.stories.js
+++ components/src/molecules/logo/Logo.stories.js
@@ -6,27 +6,30 @@ import ALogo from './Logo.vue'
 addDecorator(StoryRouter())
 storiesOf('Molecules/Logo', module)
   .addParameters({ info: true })
-  .add('Default', () => ({
-    components: { ALogo },
-    template: `
-      <a-logo
-        to="/"
-        src="/images/logo/alpaca.svg"
-        alt="Logo"
-      />
+  .add(
+    'Default',
+    () => ({
+      components: { ALogo },
+      template: `
+        <a-logo
+          to="/"
+          src="/images/logo/alpaca.svg"
+          alt="Logo"
+        />
     `
   }))
-  .add('With slots', () => ({
-    components: { ALogo },
-    template: `
-      <a-logo to="/">
-        <template #image>
-          <img 
-            src="/images/logo/alpaca.svg" 
-            alt="Logo"
-            :style="{width: '70%'}"
-          />
-        </template>
-      </a-logo>
+  .add('With slots',
+    () => ({
+      components: { ALogo },
+      template: `
+        <a-logo to="/">
+          <template #image>
+            <img 
+              src="/images/logo/alpaca.svg" 
+              alt="Logo"
+              :style="{width: '70%'}"
+            />
+          </template>
+        </a-logo>
     `
   }))
