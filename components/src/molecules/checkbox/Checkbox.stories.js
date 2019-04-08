741f900e32540e70de06c043b31a90e3d3612b30
Paweł Szafrański <pawel.szafranski@snow.dog>
2019-04-08 12:54:48 +0200

    Removed useless checkbox props

--- components/src/molecules/checkbox/Checkbox.stories.js
+++ components/src/molecules/checkbox/Checkbox.stories.js
@@ -1,13 +1,10 @@
 import { storiesOf } from '@storybook/vue'
-
-import ACheckbox from './Checkbox.vue'
 import { action } from '@storybook/addon-actions'
 
-// Preview styles
-import './preview/styles.css'
+import ACheckbox from './Checkbox.vue'
+import AIcon from './../../atoms/icon/Icon.vue'
 
 const defaultData = {
-  components: { ACheckbox },
   data: () => {
     return {
       status: true
@@ -24,6 +21,7 @@ storiesOf('Molecules/Checkbox', module)
     'Default',
     () => ({
       ...defaultData,
+      components: { ACheckbox },
       template: `
         <a-checkbox
           id="checkbox-default"
@@ -41,19 +39,28 @@ storiesOf('Molecules/Checkbox', module)
     'With slot',
     () => ({
       ...defaultData,
+      components: {
+        ACheckbox,
+        AIcon
+      },
       template: `
         <a-checkbox
           id="checkbox-slots"
           name="checkbox-slots"
-          input-class="checkbox__field-custom"
+          input-class="checkbox__field"
           v-model="status"
           value="some value"
           @input="onChange"
         >
           <template #label>
+            <a-icon
+              icon="checked"
+              class="checkbox__icon"
+            />
             <label
               for="checkbox-slots"
-              class="checkbox__label-custom"
+              class="checkbox__label"
+              style="color: #0000ff;"
             >
               I hereby agree for processing my personal data.
             </label>
