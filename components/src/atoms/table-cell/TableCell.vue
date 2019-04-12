9783c3686f2e0271762b6aae3350d58b086e2091
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-12 08:11:41 +0200

    #52304 - Move table component to atoms

---
+++ components/src/atoms/table-cell/TableCell.vue
@@ -0,0 +1,5 @@
+<template src="./TableCell.html" />
+
+<script src="./TableCell.js"/>
+
+<style lang="scss" src="./TableCell.scss"/>
---
+++ components/src/atoms/table/Table.vue
@@ -0,0 +1,6 @@
+<template src="./Table.html" />
+
+<script src="./Table.js"/>
+
+<style lang="scss" src="./Table.scss"/>
+
