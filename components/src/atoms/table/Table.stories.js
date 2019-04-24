56d5112b09bf46a91b17bafd03f1d8d058fc2e6a
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-24 20:08:15 +0200

    #52304 - Refactor mocks

--- components/src/atoms/table/Table.stories.js
+++ components/src/atoms/table/Table.stories.js
@@ -4,6 +4,10 @@ import ATable from './Table.vue'
 import ATableRow from '../table-row/TableRow.vue'
 import ATableCell from '../table-cell/TableCell.vue'
 
+import bodyRows from './mocks/bodyRows.json'
+import headCells from './mocks/headCells.json'
+import footCells from './mocks/footCells.json'
+
 const info = `
   ---
   #### Use these modifiers with \`.table\` class.
@@ -14,86 +18,9 @@ const info = `
 const defaultData = {
   components: { ATable, ATableRow, ATableCell },
   data: () => ({
-    bodyRows: [
-      [
-        {
-          'mobileHeading': 'Mobile heading 1',
-          'text': 'Example content 1',
-          'id': 'rowId1'
-        },
-        {
-          'mobileHeading': 'Mobile heading 2',
-          'text': 'Example content 2',
-          'id': 'rowId2'
-        },
-        {
-          'mobileHeading': 'Mobile heading 3',
-          'text': 'Example content 3',
-          'id': 'rowId3'
-        }
-      ],
-      [
-        {
-          'mobileHeading': 'Mobile heading 4',
-          'text': 'Example content 4',
-          'id': 'rowId4'
-        },
-        {
-          'mobileHeading': 'Mobile heading 5',
-          'text': 'Example content 5',
-          'id': 'rowId5'
-        },
-        {
-          'mobileHeading': 'Mobile heading 6',
-          'text': 'Example content 6',
-          'id': 'rowId6'
-        }
-      ],
-      [
-        {
-          'mobileHeading': 'Mobile heading 7',
-          'text': 'Example content 7',
-          'id': 'rowId7'
-        },
-        {
-          'mobileHeading': 'Mobile heading 8',
-          'text': 'Example content 8',
-          'id': 'rowId8'
-        },
-        {
-          'mobileHeading': 'Mobile heading 9',
-          'text': 'Example content 9',
-          'id': 'rowId9'
-        }
-      ]
-    ],
-    headCells: [
-      {
-        'text': 'Example heading 1',
-        'id': 'headId1'
-      },
-      {
-        'text': 'Example heading 2',
-        'id': 'headId2'
-      },
-      {
-        'text': 'Example heading 3',
-        'id': 'headId3'
-      }
-    ],
-    footCells: [
-      {
-        'colspan': 2,
-        'text': 'Footer content 1-2',
-        'id': 'id1'
-      },
-      {
-        'colspan': 1,
-        'text': 'Footer content 3',
-        'id': 'id2'
-      }
-    ]
-
+    bodyRows,
+    headCells,
+    footCells
   })
 }
 
@@ -110,7 +37,7 @@ storiesOf('Atoms/Table', module)
               tag="th"
               v-for="headCell in headCells"
               :key="headCell.id"
-              :scope="headCell.scope"
+              scope="col"
             >
               {{ headCell.text }}
             </a-table-cell>
@@ -135,7 +62,7 @@ storiesOf('Atoms/Table', module)
             <a-table-cell
               v-for="footCell in footCells"
               :key="footCell.id"
-              :colspan="footCell.colspan"
+              colspan="2"
             >
               {{ footCell.text }}
             </a-table-cell>
@@ -181,11 +108,16 @@ storiesOf('Atoms/Table', module)
         <tfoot>
           <a-table-row>
             <a-table-cell
-              v-for="footCell in footCells"
-              :key="footCell.id"
-              :colspan="footCell.colspan"
+              :key="footCells[0].id"
+              colspan="1"
             >
-              {{ footCell.text }}
+              {{ footCells[0].text }}
+            </a-table-cell>
+            <a-table-cell
+              :key="footCells[1].id"
+              colspan="2"
+            >
+              {{ footCells[1].text }}
             </a-table-cell>
           </a-table-row>
         </tfoot>
