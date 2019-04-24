7d0a79a7e81949d719b867ed1efbd59a823901f5
Joanna Szczęch <joanna.szczech@snow.dog>
2019-04-24 14:36:42 +0200

    #48042 - Formatting

--- components/src/molecules/rate/Rate.stories.js
+++ components/src/molecules/rate/Rate.stories.js
@@ -12,92 +12,84 @@ const info = `
 
 storiesOf('Molecules/Rate', module)
   .addParameters({ info })
-  .add(
-    'Default',
-    () => ({
-      components: { ARate },
-      data: () => ({
-        rating: 2.4,
-        ratingItems: 5
-      }),
-      template: `
-        <a-rate 
-         v-model="rating"
-         :rating-items="ratingItems"
-         disabled
-         legend="Your rating"
-        />
-      `
-    })
-  )
-  .add(
-    'Rate',
-    () => ({
-      components: { ARate },
-      data: () => ({
-        rating: 1,
-        ratingItems: 4
-      }),
-      template: `
-        <a-rate 
-         v-model="rating"
-         :rating-items="ratingItems"
-         :aria-label-rate="ariaLabelRate()"
-         legend="Your rating"
-         @change="select"
-        />
-      `,
-      methods: {
-        select: action('Selected'),
-        ariaLabelRate () {
-          return `Rate option, ${this.rating} of ${this.ratingItems}. Click to vote`
-        }
+  // @vue/component
+  .add('Default', () => ({
+    components: { ARate },
+    data: () => ({
+      rating: 2.4,
+      ratingItems: 5
+    }),
+    template: `
+       <a-rate 
+        v-model="rating"
+        :rating-items="ratingItems"
+        disabled
+        legend="Your rating"
+       />
+    `
+  }))
+  // @vue/component
+  .add('Rate', () => ({
+    components: { ARate },
+    data: () => ({
+      rating: 1,
+      ratingItems: 4
+    }),
+    methods: {
+      select: action('Selected'),
+      ariaLabelRate () {
+        return `Rate option, ${this.rating} of ${this.ratingItems}. Click to vote`
       }
-    })
-  )
-  .add(
-    'Large',
-    () => ({
-      components: { ARate },
-      data: () => ({
-        rating: 2.4,
-        ratingItems: 5
-      }),
-      template: `
-        <a-rate 
-         class="rating--large"
-         v-model="rating"
-         :rating-items="ratingItems"
-         disabled
-         legend="Your rating"
-        />
-      `
-    })
-  )
-  .add(
-    'With slot',
-    () => ({
-      components: { ARate },
-      data: () => ({
-        rating: 3,
-        ratingItems: 3
-      }),
-      template: `
-        <a-rate 
-         v-model="rating"
-         :rating-items="ratingItems"
-         :aria-label-rate="ariaLabelRate()"
-         legend="Your rating"
-        >
-          <template #item="data">
-            <span>{{ data.item }}</span>
-          </template>
-        </a-rate>
-      `,
-      methods: {
-        ariaLabelRate () {
-          return `Rate option, ${this.rating} of ${this.ratingItems}. Click to vote`
-        }
+    },
+    template: `
+      <a-rate 
+       v-model="rating"
+       :rating-items="ratingItems"
+       :aria-label-rate="ariaLabelRate()"
+       legend="Your rating"
+       @change="select"
+      />
+    `
+  }))
+  // @vue/component
+  .add('Large', () => ({
+    components: { ARate },
+    data: () => ({
+      rating: 2.4,
+      ratingItems: 5
+    }),
+    template: `
+      <a-rate 
+       class="rating--large"
+       v-model="rating"
+       :rating-items="ratingItems"
+       disabled
+       legend="Your rating"
+      />
+    `
+  }))
+  // @vue/component
+  .add('With slot', () => ({
+    components: { ARate },
+    data: () => ({
+      rating: 3,
+      ratingItems: 3
+    }),
+    methods: {
+      ariaLabelRate () {
+        return `Rate option, ${this.rating} of ${this.ratingItems}. Click to vote`
       }
-    })
-  )
+    },
+    template: `
+      <a-rate 
+       v-model="rating"
+       :rating-items="ratingItems"
+       :aria-label-rate="ariaLabelRate()"
+       legend="Your rating"
+      >
+        <template #item="data">
+          <span>{{ data.item }}</span>
+        </template>
+      </a-rate>
+    `
+  }))
