c84a0e581f3b8f4cc9fd7f2bda7423a5d10a93b4
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2019-04-29 15:27:37 +0200

    #52363 - Add test for slots in InputWrapper

--- components/src/atoms/input-wrapper/InputWrapper.spec.js
+++ components/src/atoms/input-wrapper/InputWrapper.spec.js
@@ -8,4 +8,18 @@ describe('Input wrapper', () => {
     expect(wrapper.classes()).toContain('input-wrapper')
     expect(wrapper.classes().length).toBe(1)
   })
+
+  it('renders slots content when passed', () => {
+    const wrapper = mount(AInputWrapper, {
+      slots: {
+        label: `<label for="input-1">Alpaca UI</label>`,
+        default: `<input id="input-1">`
+      }
+    })
+    const label = wrapper.find('.input-wrapper > label')
+    expect(label.exists()).toBe(true)
+    expect(label.text()).toEqual('Alpaca UI')
+    const input = wrapper.find('.input-wrapper > input')
+    expect(input.exists()).toBe(true)
+  })
 })
