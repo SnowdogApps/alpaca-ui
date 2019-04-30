03afb2558a797b02b6e781b0ee4ae26cfb7c35cf
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2019-04-30 13:27:16 +0200

    #52363 - Update classes in tests

--- components/src/atoms/input/Input.spec.js
+++ components/src/atoms/input/Input.spec.js
@@ -5,7 +5,7 @@ describe('Input', () => {
   it('has default structure', () => {
     const wrapper = mount(AInput)
     expect(wrapper.is('input')).toBe(true)
-    expect(wrapper.classes()).toContain('input')
+    expect(wrapper.classes()).toContain('a-input')
     expect(wrapper.classes().length).toBe(1)
   })
 })
