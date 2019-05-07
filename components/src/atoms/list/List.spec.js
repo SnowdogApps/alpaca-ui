9163a2294aec02ab25a55dec6b37d14a7fb71c52
Joanna Szczęch <joanna.szczech@snow.dog>
2019-05-07 05:30:30 +0200

    #53158 - Add knobs, tests, prefix

---
+++ components/src/atoms/list/List.spec.js
@@ -0,0 +1,37 @@
+import { mount } from 'vue-test-utils'
+import AList from './List.vue'
+
+describe('List', () => {
+  it('has default structure', () => {
+    const wrapper = mount(AList)
+
+    expect(wrapper.is('ul')).toBe(true)
+    expect(wrapper.classes()).toContain('a-list')
+    expect(wrapper.classes().length).toBe(1)
+  })
+
+  it('renders slot text when passed', () => {
+    const wrapper = mount(AList, {
+      slots: {
+        default: `
+          <li>List default text</li>
+        `
+      }
+    })
+
+    expect(wrapper.find('.a-list li').exists()).toBe(true)
+    expect(wrapper.find('.a-list li').text()).toEqual('List default text')
+  })
+
+  it('should be generated with the `ol` passed as tag', () => {
+    const wrapper = mount(AList, {
+      propsData: {
+        tag: 'ol'
+      }
+    })
+
+    expect(wrapper.is('ol')).toBe(true)
+    expect(wrapper.classes()).toContain('a-list')
+    expect(wrapper.props().tag).toBe('ol')
+  })
+})
