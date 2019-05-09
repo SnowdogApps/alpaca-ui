8046492337e20e14dd60ae23a4dfeefe378fcb8e
Paweł Szafrański <pawel.szafranski@snow.dog>
2019-05-09 19:29:32 +0200

    #53154 - loader - added unit tests and knobs

--- components/src/atoms/loader/Loader.stories.js
+++ components/src/atoms/loader/Loader.stories.js
@@ -1,21 +1,29 @@
 import { storiesOf } from '@storybook/vue'
+import { select } from '@storybook/addon-knobs'
 
 import ALoader from './Loader.vue'
 
 const info = `
   ------
-  #### Use these modifiers with \`.a-loader\` class:
+  Check **Knobs** tab to edit component properties dynamically. Below list of available BEM modifiers.
   - \`.a-loader--visible\` - Selector to display the loader
   ------
 `
+
+const bemModifiers = [null, 'a-loader--visible']
 
 storiesOf('Atoms/Loader', module)
   .addParameters({ info })
   .add('Default', () => ({
     components: { ALoader },
+    props: {
+      classKnobs: {
+        default: select('BEM Modifier', bemModifiers, 'a-loader--visible')
+      }
+    },
     template: `
       <a-loader
-        class="a-loader--visible"
+        :class="classKnobs"
         aria-label="Do not refresh the page"
       />
     `
