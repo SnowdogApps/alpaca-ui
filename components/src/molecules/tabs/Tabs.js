9c193f2a683b6e4bcacde8dc027092115ca11a3d
Joanna Szczęch <joanna.szczech@snow.dog>
2019-05-10 16:11:17 +0200

    #53294 - Add key-codes

--- components/src/molecules/tabs/Tabs.js
+++ components/src/molecules/tabs/Tabs.js
@@ -1,4 +1,5 @@
 import AButton from '../../atoms/button/Button.vue'
+import KeyCodes from '../../utils/key-codes'
 
 // @vue/component
 export default {
@@ -13,6 +14,42 @@ export default {
       this.tabs.forEach(tab => {
         tab.isActive = (tab.name === selectedTab.name)
       })
+    },
+    handleEvt (evt) {
+      function stop () {
+        evt.preventDefault()
+        evt.stopPropagation()
+      }
+      const type = evt.type
+      const key = evt.keyCode
+      const shift = evt.shiftKey
+      console.log(type, key, shift)
+
+      if (type === 'click') {
+        stop()
+        this.$emit('click', evt)
+      } else if (type === 'keydown' && key === KeyCodes.SPACE) {
+        // In keynav mode, SPACE press will also trigger a click/select
+        stop()
+        this.$emit('click', evt)
+      } else if (type === 'keydown') {
+        // For keyboard navigation
+        if (key === KeyCodes.UP || key === KeyCodes.LEFT || key === KeyCodes.HOME) {
+          stop()
+          if (shift || key === KeyCodes.HOME) {
+            this.$emit('first', evt)
+          } else {
+            this.$emit('prev', evt)
+          }
+        } else if (key === KeyCodes.DOWN || key === KeyCodes.RIGHT || key === KeyCodes.END) {
+          stop()
+          if (shift || key === KeyCodes.END) {
+            this.$emit('last', evt)
+          } else {
+            this.$emit('next', evt)
+          }
+        }
+      }
     }
   }
 }
