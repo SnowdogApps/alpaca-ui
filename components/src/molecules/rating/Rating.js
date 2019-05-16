7cda97d9cdcac32c186443b1dafe99b39297bcfd
Joanna Szczęch <joanna.szczech@snow.dog>
2019-05-16 13:15:09 +0200

    #48042 - Add possibility for add custom icon and color

--- components/src/molecules/rating/Rating.js
+++ components/src/molecules/rating/Rating.js
@@ -1,6 +1,10 @@
+import AButton from '../../atoms/button/Button.vue'
+import AIcon from '../../atoms/icon/Icon.vue'
+
 export default {
   components: {
-
+    AButton,
+    AIcon
   },
   props: {
     items: {
@@ -10,6 +14,52 @@ export default {
     icon: {
       type: String,
       default: 'star'
+    },
+    iconBorder: {
+      type: String,
+      default: null
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    },
+    color: {
+      type: String,
+      default: '#fab216'
+    },
+    unselectColor: {
+      type: String,
+      default: '#c9c9c9'
+    }
+  },
+  data () {
+    return {
+      isActiveLocal: this.isActive,
+      localItems: Array.from(Array(this.items), (x, index) => index + 1)
+        .map(el => ({ val: el, isActive: false }))
+    }
+  },
+  methods: {
+    setActive (item, isActive) {
+      this.localItems.map(el => {
+        if (item.val >= el.val) {
+          el.isActive = true
+        } else {
+          el.isActive = false
+        }
+      })
+      console.log(this.localItems)
+    },
+    select (value, item) {
+      this.isActiveLocal = value
+    },
+    showIcon (item) {
+      if ((item.isActive && !this.iconBorder) || (item.isActive && this.iconBorder) || (!item.isActive && !this.iconBorder)) {
+        return this.icon
+      }
+      if (!item.isActive && this.iconBorder) {
+        return this.iconBorder
+      }
     }
   }
 }
---
+++ components/src/molecules/rating/Rating.stories.js
@@ -0,0 +1,35 @@
+import { storiesOf } from '@storybook/vue'
+
+import ARating from './Rating.vue'
+
+storiesOf('Molecules/Rating', module)
+  .addParameters({ info: true })
+  // @vue/component
+  .add('Default', () => ({
+    components: { ARating },
+    template: `
+      <a-rating
+        :items="4"
+        icon-border="star-border"
+      />
+    `
+  }))
+  .add('With custom ikon and color', () => ({
+    components: { ARating },
+    template: `
+      <a-rating
+        :items="4"
+        icon="heart"
+        color="red"
+      />
+    `
+  }))
+  .add('With custom ikon and no border', () => ({
+    components: { ARating },
+    template: `
+      <a-rating
+        :items="4"
+        color="red"
+      />
+    `
+  }))
