480db9d78c9926b2d32ca0e4fc3965042d158a75
Joanna Szczęch <joanna.szczech@snow.dog>
2019-05-16 07:30:27 +0200

    #48042 - Add new component for rating

---
+++ components/src/molecules/rating/Rating.js
@@ -0,0 +1,15 @@
+export default {
+  components: {
+
+  },
+  props: {
+    items: {
+      type: Number,
+      default: 5
+    },
+    icon: {
+      type: String,
+      default: 'star'
+    }
+  }
+}
