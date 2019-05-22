3038adcfcb47b1be598418032798555333f207e5
Robert Wozniak <30414211+robwozniak@users.noreply.github.com>
2019-05-22 16:10:24 +0200

    #52364 - Add with slots story for product list item component

--- components/src/molecules/product-list-item/ProductListItem.stories.js
+++ components/src/molecules/product-list-item/ProductListItem.stories.js
@@ -73,3 +73,53 @@ storiesOf('Molecules/Product List Item', module)
       />
     `
   }))
+  .add('With slots', () => ({
+    ...defaultData,
+    template: `
+      <a-product-list-item
+        :id="id"
+        name="Ingrid Running Jacket"
+        imageUrl="/images/product/cart-product-160_160.jpg"
+        price=""
+        specialPrice="$100,00"
+        oldPrice="$299,99"
+        url="#"
+        :options="options"
+        @remove="removeMethod"
+      >
+        <template #image>
+          <img
+            src="/images/product/cart-product-160_160.jpg"
+            alt="Sample Image"
+            style="border: 1px dashed #000; border-radius: 50%;"
+          />
+        </template>
+        <template #name>
+          <strong>Product:</strong> Ingrid Running Jacket
+        </template>
+        <template #price>
+          <div style="margin-top: 10px; flex-basis: 100%;">
+            <strong style="color: #00ff00;">$100,00</strong>
+            <small style="text-decoration: line-through;">$299,99"</small>
+          </div>
+        </template>
+        <template #option="data">
+          <li style="margin-top: 10px;">
+            {{ data.option.label }} : {{ data.option.value }}
+          </li>
+        </template>
+        <template #quantity>
+          <input
+            type="number"
+            name="qty"
+            min="10"
+            max="100"
+            value="11"
+          >
+        </template>
+        <template #removeAction>
+          <button onclick="alert('To remove!')">Delete!</button>
+        </template>
+      </a-product-list-item>
+    `
+  }))
