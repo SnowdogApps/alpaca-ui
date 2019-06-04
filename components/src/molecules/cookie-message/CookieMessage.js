af79012b9386843d5f95b26c5ad83326bc6fba53
Joanna Szczęch <joanna.szczech@snow.dog>
2019-06-04 10:16:12 +0200

    #53585 - Remove connect with localStorage

--- components/src/molecules/cookie-message/CookieMessage.js
+++ components/src/molecules/cookie-message/CookieMessage.js
@@ -9,13 +9,6 @@ export default {
   },
   props: {
     /**
-     * Storage name
-     */
-    storageName: {
-      type: String,
-      default: 'cookie'
-    },
-    /**
      * Text for close button
      */
     closeButton: {
@@ -37,28 +30,21 @@ export default {
       default: null
     }
   },
-  data: () => {
-    return {
-      isOpen: false
-    }
-  },
-  mounted () {
-    if (localStorage.getItem(this.storageName) !== 'closed') {
-      this.isOpen = true
-    }
-  },
   methods: {
     closeBar () {
-      this.isOpen = false
-
       const focusable = this.$root.$el.querySelectorAll('button:not([disabled]), a[href], area[href] input:not([disabled]), select:not([disabled]), textarea:not([disabled]), *[tabindex]:not([tabindex="-1"]), object, embed, *[contenteditable]')
 
-      localStorage.setItem(this.storageName, 'closed')
       // after closing message box move focus on first focusable element on the page
       if (focusable.length) {
         let firstFocusable = [...focusable].find(el => el.offsetParent !== null)
         firstFocusable.focus()
       }
+
+      /**
+       * Triggered close button
+       * @type {Event}
+       */
+      this.$emit('close')
     }
   }
 }
--- components/src/molecules/cookie-message/CookieMessage.stories.js
+++ components/src/molecules/cookie-message/CookieMessage.stories.js
@@ -4,26 +4,39 @@ import { text } from '@storybook/addon-knobs'
 import ALink from '../../atoms/link/Link.vue'
 import ACookieMessage from './CookieMessage.vue'
 
+const data = {
+  props: {
+    textKnobs: {
+      default: text('Button text', 'Button text')
+    },
+    linkText: {
+      default: text('Link text', 'Example link')
+    },
+    infoText: {
+      default: text('Info text', 'Find out more about their purpose and settings in your browser. By browsing the site you are agreeing to use cookies according to your browser settings.')
+    }
+  },
+  data: () => ({
+    isOpen: true
+  }),
+  methods: {
+    close () {
+      this.isOpen = false
+    }
+  }
+}
 storiesOf('Molecules/CookieMessage', module)
   .addParameters({ info: true })
   .add('Default', () => ({
     components: { ACookieMessage, ALink },
-    props: {
-      textKnobs: {
-        default: text('Button text', 'Button text')
-      },
-      linkText: {
-        default: text('Link text', 'Example link')
-      },
-      infoText: {
-        default: text('Info text', 'Find out more about their purpose and settings in your browser. By browsing the site you are agreeing to use cookies according to your browser settings.')
-      }
-    },
+    ...data,
     template: `
       <a-cookie-message
+        v-if="isOpen"
         :close-button="textKnobs"
         aria-label="Cookie policy message"
         close-aria-label="Close cookie message"
+        @close="close"
       >
         <a-link href='#'>
           {{ linkText }}
@@ -34,19 +47,10 @@ storiesOf('Molecules/CookieMessage', module)
   }))
   .add('With slots', () => ({
     components: { ACookieMessage, ALink },
-    props: {
-      textKnobs: {
-        default: text('Button text', 'Button text')
-      },
-      linkText: {
-        default: text('Link text', 'Link text')
-      },
-      infoText: {
-        default: text('Info text', 'Info text')
-      }
-    },
+    ...data,
     template: `
       <a-cookie-message
+        v-if="isOpen"
         aria-label="Cookie policy message"
         close-aria-label="Close cookie message"
       >
@@ -55,7 +59,7 @@ storiesOf('Molecules/CookieMessage', module)
         </a-link> 
         {{ infoText }}
         <template #button="{ closeBar }">
-          <button @click="closeBar">
+          <button @click="() => { closeBar(); close(); }">
             {{ textKnobs }}
           </button>
         </template>
