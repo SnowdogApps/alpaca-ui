e9063b1f86f7d0f32200acace4d34337928c266e
Szafran <pawel.szafranski@snow.dog>
2019-06-05 09:02:54 +0200

    Added helper getClassKnobsConfig

--- src/atoms/button/Button.stories.js
+++ src/atoms/button/Button.stories.js
@@ -1,36 +1,19 @@
 import { storiesOf } from '@storybook/vue'
 import { select, text } from '@storybook/addon-knobs'
-import generateVueInfoTable from '../../../utils/vue-info-table.js'
+
+import generateVueInfoTable from '../../../utils/helpers/generate-vue-info-table.js'
+import getClassKnobsConfig from '../../../utils/helpers/get-class-knobs-config.js'
+import selectorsConfig from './Button.selectors.json'
 
 import AButton from './Button.vue'
 import AIcon from '../icon/Icon.vue'
 
-const bemModifiersConfig = [
-  {
-    name: '.a-button--secondary',
-    description: 'Selector for applying secondary styles'
-  },
-  {
-    name: '.a-button--blank',
-    description: 'Selector for applying blank styles'
-  },
-  {
-    name: '.a-button--fluid',
-    description: 'Selector for applying fluid styles (width: 100%)'
-  }
-]
-
 const info = `
   <p>Check <b>Knobs</b> tab to edit component properties dynamically.</p><br>
-  ${generateVueInfoTable(bemModifiersConfig, 'BEM modifiers')}
+  ${generateVueInfoTable(selectorsConfig, 'BEM modifiers')}
 `
 
-const bemModifiers = [
-  null,
-  'a-button--secondary',
-  'a-button--blank',
-  'a-button--fluid'
-]
+const classKnobsConfig = getClassKnobsConfig(selectorsConfig)
 
 storiesOf('Atoms/Button', module)
   .addParameters({ info })
@@ -38,7 +21,7 @@ storiesOf('Atoms/Button', module)
     components: { AButton },
     props: {
       classKnobs: {
-        default: select('BEM Modifier', bemModifiers)
+        default: select('BEM Modifier', classKnobsConfig)
       },
       textKnobs: {
         default: text('Text', 'Button text')
@@ -54,7 +37,7 @@ storiesOf('Atoms/Button', module)
     components: { AButton, AIcon },
     props: {
       classKnobs: {
-        default: select('BEM Modifier', bemModifiers)
+        default: select('BEM Modifier', classKnobsConfig)
       }
     },
     template: `
