0d85d3ea86b880b8d108f3294a5f9371ab37c0f6
Joanna Szczęch <joanna.szczech@snow.dog>
2019-06-05 10:25:01 +0200

    #53856 - Update with current alpaca-ui structure

--- src/molecules/marketing-bar/MarketingBar.js
+++ src/molecules/marketing-bar/MarketingBar.js
@@ -1,12 +1,6 @@
 // @vue/component
-import AIcon from '../../atoms/icon/Icon.vue'
-import AButton from '../../atoms/button/Button.vue'
 
 export default {
-  components: {
-    AIcon,
-    AButton
-  },
   methods: {
     closeBar () {
       /**
