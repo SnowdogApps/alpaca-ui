b2151efda9593939dd91d94b3c5ea6280d369005
talalus <talal.suhail@snow.dog>
2019-06-06 08:24:52 +0300

    #53155: heading - add knobs

--- src/atoms/heading/Heading.stories.js
+++ src/atoms/heading/Heading.stories.js
@@ -1,27 +1,64 @@
 import { storiesOf } from '@storybook/vue'
 
 import AHeading from './Heading.vue'
+import { select, text } from '@storybook/addon-knobs'
+
+const info = `
+  ---
+  Check **Knobs** tab to edit component properties dynamically. All HTML six levels of headings are available.
+  
+  ---
+`
+
+const levels = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6
+]
 
 storiesOf('Atoms/Heading', module)
-  .addParameters({ info: true })
+  .addParameters({ info })
   // @vue/component
   .add('Default', () => ({
     components: { AHeading },
+    props: {
+      levelKnobs: {
+        default: select('Heading level', levels, 1),
+        type: Number
+      },
+      textKnobs: {
+        default: text('Text', 'Heading level 1'),
+        type: String
+      }
+    },
     template: `
-      <a-heading :level="1">
-        Heading level 1
+      <a-heading :level="levelKnobs">
+        {{ textKnobs }}
       </a-heading>
     `
   }))
   // @vue/component
   .add('Custom tag', () => ({
     components: { AHeading },
+    props: {
+      levelKnobs: {
+        default: select('Heading level', levels, 4),
+        type: Number
+      },
+      textKnobs: {
+        default: text('Text', 'Span level 4'),
+        type: String
+      }
+    },
     template: `
       <a-heading
-        :level="4"
+        :level="levelKnobs"
         tag="span"
       >
-        Span level 4
+        {{ textKnobs }}
       </a-heading>
     `
   }))
