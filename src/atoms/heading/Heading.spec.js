c5bda4ad03805a81c1e1f764ede4ddf6bc0b7e85
talalus <talal.suhail@snow.dog>
2019-06-06 09:42:54 +0300

    #53155: heading - add unit test

---
+++ src/atoms/heading/Heading.spec.js
@@ -0,0 +1,34 @@
+import { mount } from '@vue/test-utils'
+import AHeading from './Heading.vue'
+
+describe('Heading', () => {
+  it('has default structure', () => {
+    const wrapper = mount(AHeading, {
+      propsData: {
+        level: 1,
+        tag: 'span'
+      }
+    })
+
+    expect(wrapper.is('span')).toBe(true)
+    expect(wrapper.classes()).toContain('a-heading')
+    expect(wrapper.classes()).toContain('a-heading--first-level')
+  })
+
+  it('renders slot text when passed', () => {
+    const wrapper = mount(AHeading, {
+      propsData: {
+        level: 1
+      },
+      slots: {
+        default: `
+          <span>Heading level 1</span>
+        `
+      }
+    })
+
+    expect(wrapper.is('h1')).toBe(true)
+    expect(wrapper.find('.a-heading span').exists()).toBe(true)
+    expect(wrapper.find('.a-heading span').text()).toEqual('Heading level 1')
+  })
+})
