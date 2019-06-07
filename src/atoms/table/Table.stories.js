b67dbfd2b5ee396ebdb34105928c9ba2482be656
Szafran <pawel.szafranski@snow.dog>
2019-06-07 10:02:42 +0200

    #54625 - added knobs for table

--- src/atoms/table/Table.stories.js
+++ src/atoms/table/Table.stories.js
@@ -1,33 +1,41 @@
 import { storiesOf } from '@storybook/vue'
+import { select } from '@storybook/addon-knobs'
 
 import bodyRows from './mocks/bodyRows.json'
 import headCells from './mocks/headCells.json'
 import footCells from './mocks/footCells.json'
 
 import generateVueInfoTable from '../../../utils/helpers/generate-vue-info-table.js'
+import getClassKnobsConfig from '../../../utils/helpers/get-class-knobs-config.js'
 import selectorsConfig from './Table.selectors.json'
 
 import ATable from './Table.vue'
 import ATableRow from '../table-row/TableRow.vue'
 import ATableCell from '../table-cell/TableCell.vue'
 
-const defaultData = {
-  components: { ATable, ATableRow, ATableCell },
-  data: () => ({
-    bodyRows,
-    headCells,
-    footCells
-  })
-}
+const classKnobsConfig = getClassKnobsConfig(selectorsConfig)
 
 storiesOf('Atoms/Table', module)
   .addParameters({
     info: generateVueInfoTable(selectorsConfig, 'BEM modifiers')
   })
   .add('Default', () => ({
-    ...defaultData,
+    components: { ATable, ATableRow, ATableCell },
+    props: {
+      classKnobs: {
+        default: select('BEM Modifier', classKnobsConfig)
+      }
+    },
+    data: () => ({
+      bodyRows,
+      headCells,
+      footCells
+    }),
     template: `
-      <a-table caption="This is a table">
+      <a-table
+        caption="This is a table"
+        :class="classKnobs"
+      >
         <thead>
           <a-table-row>
             <a-table-cell
@@ -62,58 +70,6 @@ storiesOf('Atoms/Table', module)
               colspan="2"
             >
               {{ footCell.text }}
-            </a-table-cell>
-          </a-table-row>
-        </tfoot>
-      </a-table>
-    `
-  }))
-  .add('Odd Even', () => ({
-    ...defaultData,
-    template: `
-      <a-table
-        caption="This is a table"
-        class="table--odd-even"
-      >
-        <thead>
-          <a-table-row>
-            <a-table-cell
-              tag="th"
-              v-for="headCell in headCells"
-              :key="headCell.id"
-              scope="col"
-            >
-              {{ headCell.text }}
-            </a-table-cell>
-          </a-table-row>
-        </thead>
-        <tbody>
-          <a-table-row
-            v-for="(row, i) in bodyRows"
-            :key="row.id"
-          >
-            <a-table-cell
-              v-for="bodyCell in row"
-              :key="bodyCell.id"
-              :data-th="headCells[i].text"
-            >
-              {{ bodyCell.text }}
-            </a-table-cell>
-          </a-table-row>
-        </tbody>
-        <tfoot>
-          <a-table-row>
-            <a-table-cell
-              :key="footCells[0].id"
-              colspan="1"
-            >
-              {{ footCells[0].text }}
-            </a-table-cell>
-            <a-table-cell
-              :key="footCells[1].id"
-              colspan="2"
-            >
-              {{ footCells[1].text }}
             </a-table-cell>
           </a-table-row>
         </tfoot>
