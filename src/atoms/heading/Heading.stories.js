bd278279b74171abeb7f35d461d5603ccdaf3db7
talalus <talal.suhail@snow.dog>
2019-06-10 20:59:23 +0300

    #53155: heading - remove custom tag story

--- src/atoms/heading/Heading.stories.js
+++ src/atoms/heading/Heading.stories.js
@@ -3,12 +3,7 @@ import { storiesOf } from '@storybook/vue'
 import AHeading from './Heading.vue'
 import { select, text } from '@storybook/addon-knobs'
 
-const info = `
-  ---
-  Check **Knobs** tab to edit component properties dynamically. All HTML six levels of headings are available.
-  
-  ---
-`
+const info = 'Check **Knobs** tab to edit component properties dynamically, All HTML six levels of headings are available.'
 
 const levels = [1, 2, 3, 4, 5, 6]
 
@@ -29,28 +24,6 @@ storiesOf('Atoms/Heading', module)
     },
     template: `
       <a-heading 
-        :level="levelKnobs"
-        :tag="tagKnobs"
-      >
-        {{ textKnobs }}
-      </a-heading>
-    `
-  }))
-  .add('Custom tag', () => ({
-    components: { AHeading },
-    props: {
-      levelKnobs: {
-        default: select('Heading level', levels, 4)
-      },
-      tagKnobs: {
-        default: text('Tag', 'span')
-      },
-      textKnobs: {
-        default: text('Text', 'Span level 4')
-      }
-    },
-    template: `
-      <a-heading
         :level="levelKnobs"
         :tag="tagKnobs"
       >
