d5da9c6c70bf2f84e55f1c3254bdd361c6cddb48
Joanna Szczęch <joanna.szczech@snow.dog>
2019-06-11 14:24:05 +0200

    #54502 - Add tests for logo

--- src/atoms/image/Image.vue
+++ src/atoms/image/Image.vue
@@ -1,3 +1,13 @@
 <template src="./Image.html" />
 
+<script>
+  import AImage from './Image.js'
+
+  export default {
+    name: 'AlpacaImage',
+
+    mixins: [AImage]
+  }
+</script>
+
 <style lang="scss" src="./Image.scss" />
---
+++ src/molecules/logo/Logo.spec.js
@@ -0,0 +1,34 @@
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import ALogo from './Logo.vue'
+
+describe('Button', () => {
+  it('has default structure', () => {
+    const wrapper = mount(ALogo, {
+      stubs: { 'router-link': RouterLinkStub },
+      propsData: {
+        to: '#'
+      }
+    })
+
+    expect(wrapper.is('a')).toBe(true)
+    expect(wrapper.classes()).toContain('a-logo')
+    expect(wrapper.classes().length).toBe(1)
+  })
+
+  it('renders slot text when passed', () => {
+    const wrapper = mount(ALogo, {
+      stubs: { 'router-link': RouterLinkStub },
+      propsData: {
+        to: '#'
+      },
+      slots: {
+        image: `
+          <img src="/images/logo/alpaca.svg" alt="Logo">
+        `
+      }
+    })
+
+    expect(wrapper.find('.a-logo img').exists()).toBe(true)
+    expect(wrapper.find('.a-logo img').attributes('alt')).toEqual('Logo')
+  })
+})
