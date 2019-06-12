1926f1c4e1f1d685903323c0700026a45afe422f
Joanna Szczęch <joanna.szczech@snow.dog>
2019-06-12 15:28:35 +0200

    Feature/53294 - Tabs a11y (#179)

--- src/molecules/tabs/Tabs.js
+++ src/molecules/tabs/Tabs.js
@@ -1,9 +1,12 @@
+import KeyCodes from '../../utils/key-codes'
+
 // @vue/component
 
 export default {
   data () {
     return {
-      tabs: null
+      tabs: null,
+      activeFocusedTab: 0
     }
   },
   computed: {
@@ -20,8 +23,49 @@ export default {
     selectTab (selectedTab) {
       this.$emit('click', selectedTab.name)
       this.tabs.forEach(tab => {
-        tab.isActive = tab.name === selectedTab.name
+        if (selectedTab === this.activeFocusedTab) {
+          tab.isActive = tab.name === this.tabs[this.activeFocusedTab].name
+        } else {
+          tab.isActive = tab.name === selectedTab.name
+        }
       })
+    },
+    focus (el) {
+      this.$refs[el][0].focus()
+    },
+    onKeydown (e) {
+      const key = e.keyCode
+
+      if (key === KeyCodes.TAB) {
+        e.preventDefault()
+        e.stopPropagation()
+        this.$refs.content.focus()
+      } else if (
+        (key === KeyCodes.LEFT ||
+          key === KeyCodes.UP ||
+          key === KeyCodes.HOME) &&
+        this.activeFocusedTab > 0
+      ) {
+        if (key === KeyCodes.HOME) {
+          this.focus('button_0')
+        } else {
+          this.activeFocusedTab = this.activeFocusedTab - 1
+          this.focus('button_' + this.activeFocusedTab)
+        }
+      } else if (
+        (key === KeyCodes.RIGHT ||
+          key === KeyCodes.DOWN ||
+          key === KeyCodes.END) &&
+        this.activeFocusedTab < this.tabs.length - 1
+      ) {
+        if (key === KeyCodes.END) {
+          this.focus('button_' + (this.tabs.length - 1))
+        } else {
+          this.activeFocusedTab = this.activeFocusedTab + 1
+          this.focus('button_' + this.activeFocusedTab)
+        }
+      }
+      this.selectTab(this.activeFocusedTab)
     }
   }
 }
