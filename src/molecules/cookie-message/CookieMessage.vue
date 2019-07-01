e84d00c4eccb76c888e39a1c899bf1d0405d42a3
Szafran <pawel.szafranski@snow.dog>
2019-07-01 19:24:06 +0200

    CookieMessage - moved to proper place, styles/js refactor

---
+++ src/molecules/cookie-message/CookieMessage.vue
@@ -0,0 +1,19 @@
+<template src="./CookieMessage.html" />
+
+<script>
+import ACookieMessage from './CookieMessage.js'
+
+import AIcon from '../../atoms/icon/Icon.vue'
+import AButton from '../../atoms/button/Button.vue'
+
+export default {
+  name: 'AlpacaCookieMessage',
+  components: {
+    AIcon,
+    AButton
+  },
+  mixins: [ACookieMessage]
+}
+</script>
+
+<style lang="scss" src="./CookieMessage.scss" />
