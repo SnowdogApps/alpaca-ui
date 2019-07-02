95559a03c5a0c3e10c73689d8acb55c6a9eca74d
Joanna Szczęch <joanna.szczech@snow.dog>
2019-07-02 11:51:35 +0200

    Test coverage

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,6 @@ module.exports = {
   'transform': {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
-  }
+  },
+  'collectCoverage': true
 }
