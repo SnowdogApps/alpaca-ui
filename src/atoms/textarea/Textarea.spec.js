98e169d198bef97edc90772154696754818bf491
Joanna Szczęch <joanna.szczech@snow.dog>
2019-07-03 06:07:50 +0200

    #54509 - Update tests, add prefix

--- src/atoms/textarea/Textarea.spec.js
+++ src/atoms/textarea/Textarea.spec.js
@@ -5,7 +5,33 @@ describe('Textarea', () => {
   it('has default structure', () => {
     const wrapper = mount(ATextarea)
     expect(wrapper.is('textarea')).toBe(true)
-    expect(wrapper.classes()).toContain('textarea')
+    expect(wrapper.classes()).toContain('a-textarea')
     expect(wrapper.classes().length).toBe(1)
+  })
+
+  it('is disabled when disabled = true', () => {
+    const wrapper = mount(ATextarea, {
+      propsData: {
+        disabled: true
+      }
+    })
+    const input = wrapper.find('.a-textarea')
+    expect(!!input.attributes('disabled')).toBe(true)
+    expect(input.element.disabled).toBe(true)
+  })
+
+  it('does not have attribute readonly by default', () => {
+    const wrapper = mount(ATextarea)
+    expect(wrapper.attributes('readonly')).not.toBeDefined()
+  })
+
+  it('emits an change event with single arg of value', () => {
+    const input = mount(ATextarea)
+
+    input.element.value = 'test'
+    input.trigger('input')
+    expect(input.emitted('change')).toBeDefined()
+    expect(input.emitted('change')[0].length).toEqual(1)
+    expect(input.emitted('change')[0][0]).toEqual('test')
   })
 })
