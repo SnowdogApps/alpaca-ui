dbce1192baa73b78753ddb00e9f7cfd3d7eaafc0
Robert Wozniak <30414211+robwozniak@users.noreply.github.com>
2019-07-03 19:10:08 +0200

    Remove useless heading knob from Modal component story

--- src/molecules/modal/Modal.stories.js
+++ src/molecules/modal/Modal.stories.js
@@ -159,9 +159,6 @@ storiesOf('Molecules/Modal', module)
       AInputWrapper
     },
     props: {
-      headingKnob: {
-        default: text('Modal heading', 'Modal heading')
-      },
       contentKnob: {
         default: text('Content', 'Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.iscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
       }
