2fda14e9c9efc284e85ee285f41e64d48da2275d
Szafran <pawel.szafranski@snow.dog>
2019-07-04 09:28:43 +0200

    Added @utils alias

--- src/atoms/badge/Badge.stories.js
+++ src/atoms/badge/Badge.stories.js
@@ -1,8 +1,8 @@
 import { storiesOf } from '@storybook/vue'
 import { select, text } from '@storybook/addon-knobs'
 
-import generateVueInfoTable from '../../../utils/helpers/generate-vue-info-table.js'
-import getClassKnobsConfig from '../../../utils/helpers/get-class-knobs-config.js'
+import generateVueInfoTable from '@utils/helpers/generate-vue-info-table.js'
+import getClassKnobsConfig from '@utils/helpers/get-class-knobs-config.js'
 import selectorsConfig from './Badge.selectors.json'
 
 import ABadge from './Badge.vue'
