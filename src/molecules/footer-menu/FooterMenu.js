817c0c97ed5a91ab14452b00d0a0264f312a307d
Joanna Szczęch <joanna.szczech@snow.dog>
2019-07-08 09:04:40 +0200

    #53855 - Move mobile to separate component

---
+++ src/molecules/footer-menu/FooterMenu.js
@@ -0,0 +1,30 @@
+// @vue/component
+export default {
+  props: {
+    /**
+     * Array with menu
+     */
+    menu: {
+      type: Array,
+      default: null
+    }
+  },
+  data () {
+    return {
+      isLargeScreen: window.matchMedia('(min-width: 768px)').matches
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      const breakpoint = window.matchMedia(('(min-width: 768px)'))
+      this.checkMedia(breakpoint)
+      breakpoint.addListener(this.checkMedia)
+    })
+  },
+  methods: {
+    checkMedia (e) {
+      this.isLargeScreen = e.matches
+      console.log(this.isLargeScreen)
+    }
+  }
+}
---
+++ src/molecules/footer-menu/FooterMenu.stories.js
@@ -0,0 +1,21 @@
+import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+
+import AFooterMenu from './FooterMenu.vue'
+
+import menu from './mocks/menu.json'
+
+storiesOf('Molecules/Footer menu', module)
+  .addParameters({ info: true })
+  .add('Default', () => ({
+    components: { AFooterMenu },
+    data: () => ({ menu }),
+    methods: {
+      submit: action('Submitted')
+    },
+    template: `
+      <a-footer-menu
+        :menu="menu"
+      />
+    `
+  }))
