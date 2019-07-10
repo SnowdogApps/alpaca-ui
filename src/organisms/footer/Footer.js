031756995814cf70515949f344fa1c445857dbb6
Szafran <pawel.szafranski@snow.dog>
2019-07-10 15:49:45 +0200

    #53855 - removed useless js code

--- src/organisms/footer/Footer.js
+++ src/organisms/footer/Footer.js
@@ -62,22 +62,7 @@ export default {
       default: null
     }
   },
-  data () {
-    return {
-      isLargeScreen: window.matchMedia('(min-width: 768px)').matches
-    }
-  },
-  mounted () {
-    this.$nextTick(() => {
-      const breakpoint = window.matchMedia(('(min-width: 768px)'))
-      this.checkMedia(breakpoint)
-      breakpoint.addListener(this.checkMedia)
-    })
-  },
   methods: {
-    checkMedia (e) {
-      this.isLargeScreen = e.matches
-    },
     scrollToTop () {
       window.scrollTo(0, 0)
     },
