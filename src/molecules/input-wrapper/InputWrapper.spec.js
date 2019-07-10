8540befcda8b983f5a610303b026c0511d33ec26
Joanna Szczęch <joanna.szczech@snow.dog>
2019-07-10 11:35:57 +0200

    #54499 - Fix tests

--- src/molecules/input-wrapper/InputWrapper.spec.js
+++ src/molecules/input-wrapper/InputWrapper.spec.js
@@ -3,7 +3,11 @@ import AInputWrapper from './InputWrapper.vue'
 
 describe('Input wrapper', () => {
   it('has default structure', () => {
-    const wrapper = mount(AInputWrapper)
+    const wrapper = mount(AInputWrapper, {
+      slots: {
+        default: `<input type="text" id="input-1">`
+      }
+    })
     expect(wrapper.is('div')).toBe(true)
     expect(wrapper.classes()).toContain('a-input-wrapper')
     expect(wrapper.classes().length).toBe(1)
@@ -12,6 +16,7 @@ describe('Input wrapper', () => {
   it('renders slots content when passed', () => {
     const wrapper = mount(AInputWrapper, {
       slots: {
+        default: `<input type="text" id="input-1">`,
         label: `<label for="input-1">Alpaca UI</label>`
       }
     })
