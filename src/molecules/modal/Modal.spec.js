be4d9ff44ecb32afe0ee4190166e82a17198b5b3
Robert Wozniak <30414211+robwozniak@users.noreply.github.com>
2019-07-12 15:48:25 +0200

    #54520 - Adjust tests to new Modal component structure

--- src/molecules/modal/Modal.spec.js
+++ src/molecules/modal/Modal.spec.js
@@ -10,8 +10,8 @@ describe('Modal', () => {
     expect(wrapper.classes()).toContain('a-modal')
     expect(wrapper.classes().length).toBe(1)
     expect(wrapper.find('.a-modal__mask').exists()).toBe(true)
-    expect(wrapper.find('.a-modal__header').exists()).toBe(true)
-    expect(wrapper.find('.a-modal__content').exists()).toBe(true)
+    expect(wrapper.find('.a-modal__header').exists()).toBe(false)
+    expect(wrapper.find('.a-modal__content').exists()).toBe(false)
     expect(wrapper.attributes()['aria-hidden']).toBeDefined()
     expect(wrapper.attributes()['aria-hidden']).toEqual('true')
     expect(wrapper.attributes().role).toBeDefined()
@@ -23,6 +23,7 @@ describe('Modal', () => {
   it('should render default heading if provided', () => {
     const wrapper = mount(AModal, {
       propsData: {
+        visible: true,
         heading: 'Sample heading'
       }
     })
@@ -48,6 +49,9 @@ describe('Modal', () => {
 
   it('renders slot text when passed', () => {
     const wrapper = mount(AModal, {
+      propsData: {
+        visible: true
+      },
       slots: {
         default: `<span data-test="content">Sample text</span>`,
         heading: `<h1 data-test="heading">Sample heading</h1>`,
@@ -63,17 +67,17 @@ describe('Modal', () => {
     expect(wrapper.find('.a-modal__content [data-test="close"]').text()).toEqual('Sample button')
   })
 
-  it('should change aria hidden when `show` and `hide` functions are used', () => {
-    const wrapper = mount(AModal)
+  it('should change aria hidden when visible', () => {
+    const wrapper = mount(AModal, {
+      propsData: {
+        visible: true
+      }
+    })
 
-    wrapper.vm.show()
     expect(wrapper.isVisible()).toBe(true)
     expect(wrapper.attributes()['aria-hidden']).toBeDefined()
     expect(wrapper.attributes()['aria-hidden']).toEqual('false')
-    wrapper.vm.hide()
-    expect(wrapper.isVisible()).toBe(false)
-    expect(wrapper.attributes()['aria-hidden']).toBeDefined()
-    expect(wrapper.attributes()['aria-hidden']).toEqual('true')
+    expect(wrapper.find('.a-modal__content').exists()).toBe(true)
   })
 
   it('should do not render header if there is no heading and close button is disabled', () => {
