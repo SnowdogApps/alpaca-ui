66e22ede27e2badd047146404f986dc9fbe4029c
Szafran <pawel.szafranski@snow.dog>
2019-07-15 18:55:41 +0200

    #54499 - updated tests, fixed issue with bable

--- src/molecules/input/Input.spec.js
+++ src/molecules/input/Input.spec.js
@@ -1,75 +1,66 @@
 import { mount } from '@vue/test-utils'
-import AInput1 from './Input.vue'
+import AInput from './Input.vue'
 
 describe('Input', () => {
   it('has default structure', () => {
-    const wrapper = mount(AInput1)
+    const wrapper = mount(AInput, {
+      propsData: {
+        id: 'input-id'
+      }
+    })
+    const input = wrapper.find('.a-input__field')
 
     expect(wrapper.is('div')).toBe(true)
-    // expect(wrapper.classes()).toContain('a-input')
-    // expect(wrapper.classes().length).toBe(1)
-    // expect(input.is('input')).toBe(true)
-    // expect(input.classes()).toContain('a-input__field')
-    // expect(input.classes().length).toBe(1)
+    expect(wrapper.classes()).toContain('a-input')
+    expect(wrapper.classes().length).toBe(1)
+
+    expect(input.is('input')).toBe(true)
+    expect(input.classes()).toContain('a-input__field')
+    expect(input.classes().length).toBe(1)
+    expect(input.attributes().id).toBeDefined()
+    expect(input.attributes().id).toBe('input-id')
   })
 
-  // it('renders text input when type set to text', () => {
-  //   const wrapper = mount(AInput, {
-  //     propsData: {
-  //       type: 'text'
-  //     }
-  //   })
-  //   const input = wrapper.find('.a-input')
-  //   expect(input.attributes('type')).toBe('text')
-  // })
+  it('renders email input when type set to email', () => {
+    const wrapper = mount(AInput, {
+      propsData: {
+        id: 'input-id',
+        type: 'email'
+      }
+    })
 
-  // it('is disabled when disabled = true', () => {
-  //   const wrapper = mount(AInput, {
-  //     propsData: {
-  //       disabled: true
-  //     }
-  //   })
-  //   const input = wrapper.find('.a-input')
-  //   expect(!!input.attributes('disabled')).toBe(true)
-  //   expect(input.element.disabled).toBe(true)
-  // })
+    const input = wrapper.find('.a-input__field')
+    expect(input.attributes('type')).toBe('email')
+  })
 
-  // it('emits an input event', () => {
-  //   const wrapper = mount(AInput)
-  //   const textInput = wrapper.find('input')
-  //   textInput.setValue('Sample text')
-  //   textInput.trigger('input')
+  it('renders slots content when passed', () => {
+    const wrapper = mount(AInput, {
+      propsData: {
+        id: 'input-id'
+      },
+      slots: {
+        label: `<label for="input-id">Alpaca UI</label>`
+      }
+    })
 
-  //   expect(wrapper.emitted('input')).toBeDefined()
-  //   expect(wrapper.emitted().input[0].length).toEqual(1)
-  //   expect(wrapper.emitted().input[0][0]).toEqual('Sample text')
-  // })
+    const label = wrapper.find('.a-input > label')
+
+    expect(label.exists()).toBe(true)
+    expect(label.text()).toEqual('Alpaca UI')
+  })
+
+  it('emits an input event', () => {
+    const wrapper = mount(AInput, {
+      propsData: {
+        id: 'input-id'
+      }
+    })
+
+    const textInput = wrapper.find('input')
+    textInput.setValue('Sample text')
+
+    expect(wrapper.emitted('input')).toBeTruthy()
+    expect(wrapper.emitted().input[0].length).toBe(1)
+    expect(wrapper.emitted().input[0][0]).toEqual('Sample text')
+  })
 })
-
-// import { mount } from '@vue/test-utils'
-// import AInputWrapper from './InputWrapper.vue'
-
-// describe('Input wrapper', () => {
-//   it('has default structure', () => {
-//     const wrapper = mount(AInputWrapper, {
-//       slots: {
-//         default: `<input type="text" id="input-1">`
-//       }
-//     })
-//     expect(wrapper.is('div')).toBe(true)
-//     expect(wrapper.classes()).toContain('a-input-wrapper')
-//     expect(wrapper.classes().length).toBe(1)
-//   })
-
-//   it('renders slots content when passed', () => {
-//     const wrapper = mount(AInputWrapper, {
-//       slots: {
-//         default: `<input type="text" id="input-1">`,
-//         label: `<label for="input-1">Alpaca UI</label>`
-//       }
-//     })
-//     const label = wrapper.find('.a-input-wrapper > label')
-//     expect(label.exists()).toBe(true)
-//     expect(label.text()).toEqual('Alpaca UI')
-//   })
-// })
