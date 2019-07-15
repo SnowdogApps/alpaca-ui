64152b115ae0dc4757afe4a9a4de8d3a8920968e
Szafran <pawel.szafranski@snow.dog>
2019-07-15 18:56:10 +0200

    #54499 - fixed qty updater

--- src/molecules/quantity-update/QuantityUpdate.vue
+++ src/molecules/quantity-update/QuantityUpdate.vue
@@ -3,14 +3,12 @@
 <script>
 import AQuantityUpdate from './QuantityUpdate.js'
 
-import AInputWrapper from '../../molecules/input-wrapper/InputWrapper.vue'
 import AIcon from '../../atoms/icon/Icon.vue'
 import AButton from '../../atoms/button/Button.vue'
 import ALabel from '../../atoms/label/Label.vue'
 
 export default {
   components: {
-    AInputWrapper,
     AIcon,
     AButton,
     ALabel
