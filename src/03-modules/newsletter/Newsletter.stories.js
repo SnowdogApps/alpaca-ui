dcf3d4c9101e6cad2a70a2da85e85a22ef393fed
Szafran <pawel.szafranski@snow.dog>
2019-07-16 12:28:49 +0200

    #53855 - refactored styles and props

--- src/03-modules/newsletter/Newsletter.stories.js
+++ src/03-modules/newsletter/Newsletter.stories.js
@@ -2,19 +2,23 @@ import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 
 import ANewsletter from './Newsletter.vue'
-import newsletter from './mocks/newsletter.json'
 
 storiesOf('Modules/Newsletter', module)
   .add('Default', () => ({
     components: { ANewsletter },
     data: () => ({
-      newsletter,
       checkbox: {
         status: true,
         name: 'newsletter',
         id: 'newsletter-agreement',
         value: 'newsletter-agreement-value',
         label: 'I agree to Terms and conditions and I am happy to receive your newsletter with all your promotions.'
+      },
+      input: {
+        placeholder: 'Enter your email address',
+        id: 'newsletter-id',
+        name: 'newsletter-email',
+        label: 'Email'
       }
     }),
     methods: {
@@ -23,10 +27,10 @@ storiesOf('Modules/Newsletter', module)
     template: `
       <div style="background: #393243; padding: 0 10px;">
         <a-newsletter
-          :heading="newsletter.heading"
-          :input="newsletter.input"
-          button-text="Submit"
+          :input="input"
           :checkbox="checkbox"
+          heading="Sing up to our twisted newsletter"
+          button-text="Submit"
           @submit="exampleMethod"
         />
       </div>
