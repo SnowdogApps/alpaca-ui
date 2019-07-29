cd62d8f0b3e3af7db33e219495a6daf7325604aa
Szafran <pawel.szafranski@snow.dog>
2019-07-30 01:14:41 +0200

    #56569 - Simplified transition expand

--- src/atoms/transition-expand/TransitionExpand.vue
+++ src/atoms/transition-expand/TransitionExpand.vue
@@ -8,34 +8,11 @@ export default {
         name: 'expand'
       },
       on: {
-        afterEnter (element) {
-          element.style.height = 'auto'
-        },
         enter (element) {
-          element.style.width = `${element.offsetWidth}px`
-          element.style.position = 'absolute'
-          element.style.visibility = 'hidden'
-          element.style.height = 'auto'
-
-          const { height } = getComputedStyle(element)
-          element.style.width = null
-          element.style.position = null
-          element.style.visibility = null
-          element.style.height = 0
-
-          // eslint-disable-next-line no-unused-expressions
-          getComputedStyle(element).height
-          setTimeout(() => {
-            element.style.height = height
-          })
+          element.style.height = `${element.scrollHeight}px`
         },
         leave (element) {
-          element.style.height = `${element.scrollHeight}px`
-          // eslint-disable-next-line no-unused-expressions
-          getComputedStyle(element).height
-          setTimeout(() => {
-            element.style.height = 0
-          })
+          element.style.height = 0
         }
       }
     }
@@ -44,20 +21,11 @@ export default {
 }
 </script>
 
-<style scoped>
-* {
-  transform: translateZ(0);
-  backface-visibility: hidden;
-  perspective: 600px;
-  will-change: height;
-}
-</style>
-
 <style>
 .expand-enter-active,
 .expand-leave-active {
   overflow: hidden;
-  transition: height 400ms ease-in-out;
+  transition: height 400ms;
 }
 
 .expand-enter,
