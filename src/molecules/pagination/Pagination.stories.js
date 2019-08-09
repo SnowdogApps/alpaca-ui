2f4953e31658c60fe659b6c9fbc36534dc42482b
Joanna Szczęch <joanna.szczech@snow.dog>
2019-08-09 13:11:27 +0200

    #56489 - Add knobs

--- src/molecules/pagination/Pagination.stories.js
+++ src/molecules/pagination/Pagination.stories.js
@@ -1,14 +1,23 @@
 import { storiesOf } from '@storybook/vue'
+import { number } from '@storybook/addon-knobs'
 
 import Pager from './Pagination.vue'
 
 storiesOf('Molecules/Pagination', module)
-  // @vue/component
+  .addParameters({ info: true })
   .add('Default', () => ({
     components: { Pager },
     data: () => ({
       page: 1
     }),
+    props: {
+      limitPageKnobs: {
+        default: number('Limit per page', 3)
+      },
+      totalSizeKnobs: {
+        default: number('Total size', 20)
+      }
+    },
     computed: {
       currentPage () {
         return this.page
@@ -23,8 +32,8 @@ storiesOf('Molecules/Pagination', module)
       <pager
         @update:page="(page) => {this.setCurrentPage(page)}"
         :page="page"
-        :limit="3"
-        :totalSize="55"
+        :limit="limitPageKnobs"
+        :totalSize="totalSizeKnobs"
       />
     `
   }))
