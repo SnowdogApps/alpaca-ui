9b137dc6bda55ba7a9c49dddb33bb647bae58898
Joanna Szczęch <joanna.szczech@snow.dog>
2019-08-20 10:00:58 +0200

    #54475 - Remove sprite from story

--- src/molecules/rating/Rating.stories.js
+++ src/molecules/rating/Rating.stories.js
@@ -117,8 +117,11 @@ storiesOf('Molecules/Rating', module)
             role="img"
             :style="[(data.item.isActive || data.item.selected) && {fill: 'red'}, {width: '24px', height: '24px'}]"
           >
-            <title>heart</title>
-            <use xlink:href="/assets/icons/sprite.svg#heart" />
+            <g>
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </g>
           </svg>
         </template>
       </a-rating>
