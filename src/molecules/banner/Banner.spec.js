3b58a361493a2aeb26d1134a4c971208ceedc3aa
Joanna Szczęch <joanna.szczech@snow.dog>
2019-09-06 10:34:25 +0200

    #53149 - Update tests

--- src/molecules/banner/Banner.spec.js
+++ src/molecules/banner/Banner.spec.js
@@ -5,7 +5,8 @@ describe('Banner', () => {
   it('has default structure', () => {
     const wrapper = mount(ABanner, {
       propsData: {
-        image: { key: 'key', src: 'src' }
+        imageAlt: 'alt',
+        imageSrc: 'src'
       }
     })
 
@@ -17,7 +18,8 @@ describe('Banner', () => {
   it('renders named slot text when passed', () => {
     const wrapper = mount(ABanner, {
       propsData: {
-        image: { key: 'key', src: 'src' },
+        imageAlt: 'alt',
+        imageSrc: 'src',
         text: 'Title'
       },
       slots: {
@@ -32,13 +34,15 @@ describe('Banner', () => {
   it('should be generated with correct props', () => {
     const wrapper = mount(ABanner, {
       propsData: {
-        image: { key: 'key', src: 'src' },
+        imageAlt: 'alt',
+        imageSrc: 'src',
         sources: [{ srcset: 'srcset', media: 'media' }],
         text: 'Text'
       }
     })
 
-    expect(wrapper.props().image.key).toBe('key')
+    expect(wrapper.props().imageSrc).toBe('src')
+    expect(wrapper.props().imageAlt).toBe('alt')
     expect(wrapper.props().sources[0].media).toBe('media')
     expect(wrapper.props().text).toBe('Text')
   })
