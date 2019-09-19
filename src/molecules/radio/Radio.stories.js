c3f455f95925c2b79932dae713c536311cb931af
Joanna Szczęch <joanna.szczech@snow.dog>
2019-09-19 19:48:49 +0200

    #57831 - Add methods in story, refactor, formatting

--- src/molecules/radio/Radio.stories.js
+++ src/molecules/radio/Radio.stories.js
@@ -1,28 +1,37 @@
 import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
 import { text } from '@storybook/addon-knobs'
 
 import ARadio from './Radio.vue'
+
+const sampleData = {
+  data () {
+    return {
+      selected: ''
+    }
+  },
+  props: {
+    labelTextKnobs: {
+      default: text('Label', 'Option one')
+    }
+  },
+  methods: {
+    change: action('Changed')
+  }
+}
 
 storiesOf('Molecules/Radio', module)
   .addParameters({ info: true })
   .add('Default', () => ({
     components: { ARadio },
-    data () {
-      return {
-        selected: 'option_two'
-      }
-    },
-    props: {
-      labelTextKnobs: {
-        default: text('Label', 'Option one')
-      }
-    },
+    ...sampleData,
     template: `
       <div>
         <a-radio
           v-model="selected"
           name="radio1"
           value="option_one"
+          @change="change"
           id="r1"
         >
           {{ labelTextKnobs }}
@@ -32,22 +41,14 @@ storiesOf('Molecules/Radio', module)
   }))
   .add('With slot', () => ({
     components: { ARadio },
-    data () {
-      return {
-        selected: ''
-      }
-    },
-    props: {
-      labelTextKnobs: {
-        default: text('Label', 'Custom option one')
-      }
-    },
+    ...sampleData,
     template: `
       <div>
         <a-radio
           v-model="selected"
           name="radio1"
           id="id1"
+          @change="change"
           value="option_one"
         >
           <template #option="label">
