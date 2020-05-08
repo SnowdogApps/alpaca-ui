d0b26d581ea0c662a787b715efb453635dba9805
Ola <ola.frankowska@snow.dog>
2020-05-08 13:00:27 +0200

    #261 update heading styles, add BEM modifier

--- src/atoms/heading/Heading.stories.js
+++ src/atoms/heading/Heading.stories.js
@@ -3,7 +3,15 @@ import { storiesOf } from '@storybook/vue'
 import AHeading from './Heading.vue'
 import { select, text } from '@storybook/addon-knobs'
 
-const info = 'Check **Knobs** tab to edit component properties dynamically, All HTML six levels of headings are available.'
+import generateVueInfoTable from '@utils/helpers/generate-vue-info-table.js'
+import getClassKnobsConfig from '@utils/helpers/get-class-knobs-config.js'
+import selectorsConfig from './Heading.selectors.json'
+
+const info = `
+  <p>Check <b>Knobs</b> tab to edit component properties dynamically. All HTML six levels of headings are available.</p><br>
+  ${generateVueInfoTable(selectorsConfig, 'BEM modifiers')}
+`
+const classKnobsConfig = getClassKnobsConfig(selectorsConfig)
 
 const levels = [1, 2, 3, 4, 5, 6]
 
@@ -20,12 +28,16 @@ storiesOf('Atoms/Heading', module)
       },
       textKnobs: {
         default: text('Text', 'Heading level 1')
+      },
+      classKnob: {
+        default: select('BEM Modifier', classKnobsConfig)
       }
     },
     template: `
       <a-heading 
         :level="levelKnobs"
         :tag="tagKnobs"
+        :class="classKnob"
       >
         {{ textKnobs }}
       </a-heading>
