8d8a509ddda0e5ddfdbbc4eec7e3afd417ff94d4
Ola <ola.frankowska@snow.dog>
2020-06-19 10:43:09 +0200

    update heading

---
+++ src/atoms/heading/Heading.vue
@@ -0,0 +1,13 @@
+<template src="./Heading.html" />
+
+<script>
+import AHeading from './Heading.js'
+
+export default {
+  name: 'AlpacaHeading',
+
+  mixins: [AHeading]
+}
+</script>
+
+<style lang="scss" src="./Heading.scss" />
