3a956b2e796bbb11a4cfec11ee9ba15edd6faedb
Ola <ola.frankowska@snow.dog>
2020-07-03 17:01:45 +0200

    update blockquote

--- src/atoms/blockquote/Blockquote.stories.js
+++ src/atoms/blockquote/Blockquote.stories.js
@@ -1,20 +1,22 @@
-import { storiesOf } from '@storybook/vue'
 import { text } from '@storybook/addon-knobs'
 
 import ABlockquote from './Blockquote.vue'
 
-storiesOf('Atoms/Blockquote', module)
-  .addParameters({ info: true })
-  .add('Default', () => ({
-    components: { ABlockquote },
-    props: {
-      contentKnob: {
-        default: text('Blockquote content', 'Qui quis labore ad laborum laborum ipsum veniam sunt. Laboris tempor id ut commodo aute incididunt consequat esse aute irure. Et irure elit veniam proident non do commodo labore cillum irure laborum ullamco nostrud.')
-      }
-    },
-    template: `
-      <a-blockquote>
-        {{ contentKnob }}
-      </a-blockquote>
-    `
-  }))
+export default {
+  title: 'Atoms/Blockquote',
+  component: ABlockquote
+}
+
+export const Default = () => ({
+  components: { ABlockquote },
+  props: {
+    contentKnob: {
+      default: text('Blockquote content', 'Qui quis labore ad laborum laborum ipsum veniam sunt. Laboris tempor id ut commodo aute incididunt consequat esse aute irure. Et irure elit veniam proident non do commodo labore cillum irure laborum ullamco nostrud.')
+    }
+  },
+  template: `
+    <a-blockquote>
+      {{ contentKnob }}
+    </a-blockquote>
+  `
+})
--- src/atoms/blockquote/Blockquote.vue
+++ src/atoms/blockquote/Blockquote.vue
@@ -10,4 +10,4 @@ export default {
 }
 </script>
 
-<style lang="scss" src="./Blockquote.scss" />
+<style lang="css" src="./Blockquote.css" />
