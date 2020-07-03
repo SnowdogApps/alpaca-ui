d16e44c0c1eaa7afd5100dcc00e7a804d362b38e
Zuza <zuzanna.szymanda@snow.dog>
2020-07-03 14:05:38 +0200

    update label

---
+++ src/atoms/label/Label.js
@@ -0,0 +1,12 @@
+// @vue/component
+export default {
+  props: {
+    /**
+     * To use another tag instead of 'label'
+     */
+    tag: {
+      type: String,
+      default: 'label'
+    }
+  }
+}
