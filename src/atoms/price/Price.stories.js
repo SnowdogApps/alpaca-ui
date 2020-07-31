50b815477b77e95266ec18415115a319527f97b1
Ola <ola.frankowska@snow.dog>
2020-07-31 16:49:33 +0200

    update price

--- src/atoms/price/Price.stories.js
+++ src/atoms/price/Price.stories.js
@@ -1,45 +1,38 @@
-import { storiesOf } from '@storybook/vue'
 import { text } from '@storybook/addon-knobs'
 
 import APrice from './Price.vue'
 
-storiesOf('Atoms/Price', module)
-  .addParameters({ info: true })
-  .add('Default', () => ({
-    components: { APrice },
-    props: {
-      regularPriceKnobs: {
-        default: text('Regular price', '$36,00')
-      }
+export default {
+  title: 'Atoms/Price',
+  component: APrice
+}
+
+export const Default = () => ({
+  components: { APrice },
+  props: {
+    regularPriceKnobs: {
+      default: text('Regular price', '36,00')
+    }
+  },
+  template: '<a-price :regular-price="regularPriceKnobs" />'
+})
+
+export const SpecialPrice = () => ({
+  components: { APrice },
+  props: {
+    regularPriceKnobs: {
+      default: text('Regular price', '$36,00')
     },
-    template: '<a-price :regular-price="regularPriceKnobs" />'
-  }))
-  .add('Special Price', () => ({
-    components: { APrice },
-    props: {
-      regularPriceKnobs: {
-        default: text('Regular price', '$36,00')
-      },
-      specialPriceKnobs: {
-        default: text('Special price', '$19,90')
-      }
-    },
-    template: `
-      <a-price
-        :regular-price="regularPriceKnobs"
-        :special-price="specialPriceKnobs"
-        aria-label-special-price="Special price:"
-        aria-label-old-price="Old price:"
-      />
-    `
-  }))
-  .add('With slot', () => ({
-    components: { APrice },
-    template: `
-      <a-price>
-        <template #regularPrice>
-          Price: <span style="color: #2b15e0;">$123</span>
-        </template>
-      </a-price>
-    `
-  }))
+    specialPriceKnobs: {
+      default: text('Special price', '$19,90')
+    }
+  },
+  template: `
+    <a-price
+      :regular-price="regularPriceKnobs"
+      :special-price="specialPriceKnobs"
+      aria-label-special-price="On sale at:"
+      aria-label-old-price="Price reduced from:"
+    />
+  `
+})
