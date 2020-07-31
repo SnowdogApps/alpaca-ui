c7862028a1048cdb5b75072771ee62f77a5f3859
Ola <ola.frankowska@snow.dog>
2020-07-31 10:44:58 +0200

    remove language attr and empty line

--- src/atoms/container/Container.vue
+++ src/atoms/container/Container.vue
@@ -5,9 +5,8 @@ import AContainer from './Container.js'
 
 export default {
   name: 'AlpacaContainer',
-
   mixins: [AContainer]
 }
 </script>
 
-<style lang="css" src="./Container.css" />
+<style src="./Container.css" />
