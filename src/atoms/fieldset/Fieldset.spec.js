182390b937a332705c3dc604e9608d16e7e0f5ac
Ola <ola.frankowska@snow.dog>
2020-07-31 10:58:26 +0200

    fix fieldset test error

--- src/atoms/fieldset/Fieldset.spec.js
+++ src/atoms/fieldset/Fieldset.spec.js
@@ -12,7 +12,7 @@ describe('Fieldset', () => {
     expect(wrapper.element.tagName).toBe('FIELDSET')
     expect(wrapper.classes()).toContain('a-fieldset')
     expect(wrapper.classes().length).toBe(1)
-    expect(wrapper.contains('legend')).toBe(true)
+    expect(wrapper.find('legend')).exists().toBe(true)
     expect(wrapper.find('legend').text()).toEqual('Legend text')
   })
 
