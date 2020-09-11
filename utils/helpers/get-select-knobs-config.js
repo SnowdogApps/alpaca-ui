0112b7c9a649e9282e714789f9edc743621d1579
Zuza <zuzanna.szymanda@snow.dog>
2020-09-11 15:25:25 +0200

    remove null from select options list

--- utils/helpers/get-select-knobs-config.js
+++ utils/helpers/get-select-knobs-config.js
@@ -1,6 +1,5 @@
 export default (items) => {
   items = items.map((item) => item.name.replace('.', ''))
-  items.unshift(null)
   return items.reduce((obj, item) => {
     obj[item] = item
     return obj
