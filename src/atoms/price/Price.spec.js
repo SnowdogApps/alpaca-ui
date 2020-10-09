954646dc78a94ec74c4e3bc2fbc80b7eca5d7f34
Ola <ola.frankowska@snow.dog>
2020-10-09 17:14:53 +0200

    move price classes to html, update stories & UT

--- src/atoms/price/Price.spec.js
+++ src/atoms/price/Price.spec.js
@@ -6,8 +6,6 @@ describe('Price', () => {
     const wrapper = mount(APrice)
 
     expect(wrapper.element.tagName).toBe('DIV')
-    expect(wrapper.classes()).toContain('a-price')
-    expect(wrapper.classes().length).toBe(1)
   })
 
   it('renders regular price when props regularPrice passed', () => {
@@ -24,20 +22,22 @@ describe('Price', () => {
     const wrapper = mount(APrice, {
       propsData: {
         regularPrice: '$128,00',
-        specialPrice: '$99,00'
+        specialPrice: '$99,00',
+        ariaLabelOldPrice: 'Price reduced from:',
+        ariaLabelSpecialPrice: 'On sale at:'
       }
     })
 
-    const oldPrice = wrapper.find('.a-price__old')
-    const specialPrice = wrapper.find('.a-price__special')
+    const oldPrice = wrapper.find('del')
+    const specialPrice = wrapper.find('ins')
 
     expect(oldPrice.exists()).toBe(true)
-    expect(oldPrice.element.tagName).toBe('DEL')
     expect(oldPrice.text()).toEqual('$128,00')
+    expect(oldPrice.attributes('aria-label')).toEqual('Price reduced from: $128,00')
 
     expect(specialPrice.exists()).toBe(true)
-    expect(specialPrice.element.tagName).toBe('INS')
     expect(specialPrice.text()).toEqual('$99,00')
+    expect(specialPrice.attributes('aria-label')).toEqual('On sale at: $99,00')
   })
 
   it('renders named slot regularPrice when passed', () => {
@@ -57,11 +57,13 @@ describe('Price', () => {
         regularPrice: '$128,00',
         specialPrice: '$99,00',
         oldPriceCustomClass: 'old-price-custom-class',
-        specialPriceCustomClass: 'special-price-custom-class'
+        specialPriceCustomClass: 'special-price-custom-class',
       }
     })
 
-    expect(wrapper.find('.a-price__old').classes()).toContain('old-price-custom-class')
-    expect(wrapper.find('.a-price__special').classes()).toContain('special-price-custom-class')
+    expect(wrapper.find('del').text()).toEqual('$128,00')
+    expect(wrapper.find('ins').text()).toEqual('$99,00')
+    expect(wrapper.find('del').classes()).toContain('old-price-custom-class')
+    expect(wrapper.find('ins').classes()).toContain('special-price-custom-class')
   })
 })
--- src/atoms/price/Price.stories.js
+++ src/atoms/price/Price.stories.js
@@ -14,23 +14,31 @@ export const Default = () => ({
       default: text('Regular price', '36,00')
     }
   },
-  template: '<a-price :regular-price="regularPriceKnobs" />'
+  template: '<a-price :regular-price="regularPriceKnobs"/>'
 })
 
 export const SpecialPrice = () => ({
   components: { APrice },
   props: {
     regularPriceKnobs: {
-      default: text('Regular price', '$36,00')
+      default: text('Old price', '$36,00')
     },
     specialPriceKnobs: {
       default: text('Special price', '$19,90')
+    },
+    oldPriceCustomClass: {
+      default: text('Old price custom class', '')
+    },
+    specialPriceCustomClass: {
+      default: text('Special price custom class', '')
     }
   },
   template: `
     <a-price
       :regular-price="regularPriceKnobs"
       :special-price="specialPriceKnobs"
+      :old-price-custom-class="oldPriceCustomClass"
+      :special-price-custom-class="specialPriceCustomClass"
       aria-label-special-price="On sale at:"
       aria-label-old-price="Price reduced from:"
     />
