50dbfa47c00f0312304d765aeb4feff80edb95a5
Ola <ola.frankowska@snow.dog>
2021-03-27 00:09:22 +0100

    update icon component

--- src/atoms/icon/Icon.stories.js
+++ src/atoms/icon/Icon.stories.js
@@ -40,26 +40,14 @@ import AIconAttention from './templates/IconAttention.vue'
 import AIconOk from './templates/IconOk.vue'
 import AIconError from './templates/IconError.vue'
 
-const wrapperStyles = {
-  margin: '20px',
-  width: '100px',
-  display: 'flex',
-  'flex-direction': 'column',
-  'align-items': 'center'
-}
-
-const labelStyles = {
-  display: 'block',
-  'margin-top': '20px',
-  'text-align': 'center'
-}
+import getClass from '../../../utils/helpers/get-class.js'
 
 export default {
   title: 'Atoms/Icon',
   component: AIcon
 }
 
-export const Default = () => ({
+const Template = (args, { argTypes }) => ({
   components: {
     AIcon,
     AIconArrowLeft,
@@ -103,332 +91,348 @@ export const Default = () => ({
     AIconOk,
     AIconError
   },
+  mixins: [getClass],
   data: () => ({
-    wrapperStyles,
-    labelStyles
+    config: {
+      base: {
+        'icon-grid': [
+          'flex', 'flex-wrap'
+        ],
+        'icon-grid__item': [
+          'flex', 'flex-col', 'items-center',
+          'm-5', 'w-24'
+        ],
+        'icon-grid__item-label': [
+          'mt-5',
+          'text-center'
+        ]
+      }
+    }
   }),
   template: `
-    <div style="display: flex; flex-wrap: wrap; padding: 10px;">
-      <div :style="wrapperStyles">
+    <div :class="getClass('icon-grid')">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Arrow Left icon">
           <a-icon-arrow-left />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           arrow_left
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Arrow Right icon">
           <a-icon-arrow-right />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           arrow_right
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Add icon">
           <a-icon-add />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           add
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Remove icon">
           <a-icon-remove />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           remove
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Check icon">
           <a-icon-check />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           check
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Close icon">
           <a-icon-close />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           close
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Delete icon">
           <a-icon-delete />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           delete
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Equalizer icon">
           <a-icon-equalizer />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           equalizer
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Expand more icon">
           <a-icon-expand-more />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           expand_more
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Expand less icon">
           <a-icon-expand-less />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           expand_less
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Angle left">
           <a-icon-angle-left />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           angle_left
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Angle right">
           <a-icon-angle-right />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           angle_right
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Account icon">
           <a-icon-person />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           person
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Search icon">
           <a-icon-search />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           search
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Favorite icon">
           <a-icon-favorite />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           favorite
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Favorite border icon">
           <a-icon-favorite-border />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           favorite_border
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
       <a-icon title="Compare icon">
           <a-icon-compare />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           favorite_border
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Star icon">
           <a-icon-star />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           star
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Star border icon">
           <a-icon-star-border />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           star_border
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Grid icon">
           <a-icon-grid />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           grid
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="List icon">
           <a-icon-list />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           list
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Shopping cart icon">
           <a-icon-shopping-cart />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           shooping_cart
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Edit icon">
           <a-icon-edit />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           edit
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Share icon">
           <a-icon-share />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           share
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Phone icon">
           <a-icon-phone />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           phone
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Envelope icon">
           <a-icon-envelope />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           envelope
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Mobile menu icon">
           <a-icon-mobile-menu />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           mobile menu
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Visibility icon">
             <a-icon-visibility />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           visibility
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Visibility off icon">
           <a-icon-visibility-off />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           visibility
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Unchecked icon">
           <a-icon-unchecked />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           unchecked
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Checked icon">
           <a-icon-checked />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           checked
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Youtube icon">
           <a-icon-youtube />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           youtube
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Facebook icon">
           <a-icon-facebook />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           facebook
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Twitter icon">
           <a-icon-twitter />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           twitter
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Thumb up icon">
           <a-icon-thumb-up />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           thumb_up
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Thumb down icon">
           <a-icon-thumb-down />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           thumb_down
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="More icon">
           <a-icon-more />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           more
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Attention icon">
           <a-icon-attention />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           attention
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Ok icon">
           <a-icon-ok />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           ok
         </span>
       </div>
-      <div :style="wrapperStyles">
+      <div :class="getClass('icon-grid__item')">
         <a-icon title="Error icon">
           <a-icon-error />
         </a-icon>
-        <span :style="labelStyles">
+        <span :class="getClass('icon-grid__item-label')">
           error
         </span>
       </div>
     </div>
   `
 })
+
+export const Default = Template.bind({})
