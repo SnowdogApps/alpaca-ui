520f4d80afa00428d4ae501502c8f884a78e8f14
Zuza <zuzanna.szymanda@snow.dog>
2021-07-16 13:14:06 +0200

    leave only one story with slots

--- src/atoms/list/List.stories.js
+++ src/atoms/list/List.stories.js
@@ -1,10 +1,6 @@
 import AList from './List.vue'
 import AListItem from '@atoms/list-item/ListItem.vue'
 import ALink from '@atoms/link/Link.vue'
-import AIcon from '@atoms/icon/Icon.vue'
-import AIconPerson from '@atoms/icon/templates/IconPerson.vue'
-import AIconCheck from '@atoms/icon/templates/IconCheck.vue'
-import AIconPhone from '@atoms/icon/templates/IconPhone.vue'
 
 export default {
   title: 'Atoms/List',
@@ -55,8 +51,8 @@ const BaseTemplate = (args, { argTypes }) => ({
 
 export const Default = BaseTemplate.bind({})
 
-// with links list
-const WithLinksTemplate = (args, { argTypes }) => ({
+// with slots list
+const WithSlotsTemplate = (args, { argTypes }) => ({
   components: { AList, AListItem, ALink },
   props: Object.keys(argTypes),
   template: `
@@ -77,34 +73,4 @@ const WithLinksTemplate = (args, { argTypes }) => ({
   `
 })
 
-export const WithLinks = WithLinksTemplate.bind({})
-
-// with icons list
-const WithIconsTemplate = (args, { argTypes }) => ({
-  components: { AList, AListItem, AIcon, AIconPerson, AIconCheck, AIconPhone },
-  props: Object.keys(argTypes),
-  template: `
-    <a-list
-      :tag="tag"
-      :variant="variant"
-    >
-      <a-list-item>
-        <a-icon title="Account icon">
-          <a-icon-person />
-        </a-icon>
-      </a-list-item>
-      <a-list-item>
-        <a-icon title="Check icon">
-          <a-icon-check />
-        </a-icon>
-      </a-list-item>
-      <a-list-item>
-        <a-icon title="Phone icon">
-          <a-icon-phone />
-        </a-icon>
-      </a-list-item>
-    </a-list>
-  `
-})
-
-export const WithIcons = WithIconsTemplate.bind({})
+export const WithSlots = WithSlotsTemplate.bind({})
