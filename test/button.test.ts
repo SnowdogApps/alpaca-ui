5e80718d4b31bc62f88edfe60a89e1e5c4032639
Paweł <pawel.szafranski@snow.dog>
2022-07-01 14:56:57 +0200

    Setup vitest

---
+++ test/button.test.ts
@@ -0,0 +1,14 @@
+import { mount } from "@vue/test-utils";
+import AButton from "../src/AButton.vue";
+
+test("mount component", async () => {
+  expect(AButton).toBeTruthy();
+
+  const wrapper = mount(AButton, {
+    props: {
+      color: 'red',
+    },
+  })
+  
+  expect(wrapper.html()).toMatchSnapshot()
+});
