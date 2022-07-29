4142bcd3155cc2ce160a755572371b99f470194d
Ola <ola.frankowska@snow.dog>
2022-07-29 14:58:24 +0200

    Update button

--- src/AButton.story.vue
+++ src/AButton.story.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
 import { hstEvent } from 'histoire/client'
 import AButton from './AButton.vue'
+import { variants } from './AButton.vue'
 </script>
 
 <template>
-  <Story>
-    <AButton
-      type="button"
-      @click="hstEvent('click', $event)"
+  <Story :layout="{ type: 'grid' }">
+    <Variant
+      v-for="variant in variants"
+      :key="variant"
+      :title="variant"
     >
-      Button
-    </AButton>
+      <AButton
+        :variant="variant"
+        @click="hstEvent('click', $event)"
+      >
+        Add to cart
+      </AButton>
+    </Variant>
+    <Variant title="Link">
+      <AButton
+        tag="a"
+        href="https://snow.dog/"
+      >
+        Link
+      </AButton>
+    </Variant>
   </Story>
 </template>
--- src/AButton.vue
+++ src/AButton.vue
@@ -1,21 +1,47 @@
+<script lang="ts">
+export const variants = [
+  'primary',
+  'secondary',
+  'tertiary',
+  'transparent',
+] as const
+</script>
+
 <script setup lang="ts">
-defineProps({
-  color: {
-    type: String,
-    default: null,
-  },
+type ButtonType = 'button' | 'submit' | 'reset'
+
+type ButtonProps = {
+  variant: typeof variants[number]
+  tag: 'button' | 'a'
+  type?: ButtonType
+  disabled?: boolean
+}
+
+withDefaults(defineProps<ButtonProps>(), {
+  variant: 'primary',
+  tag: 'button',
+  type: 'button',
+  disabled: undefined,
 })
 </script>
 
 <template>
-  <button
-    class="rounded-md px-6 py-3"
-    :class="{
-      'bg-gray-200': color == null,
-      'bg-green-200': color === 'green',
-      'bg-red-200': color === 'red',
-    }"
+  <component
+    :is="tag"
+    :type="tag === 'button' ? type : undefined"
+    :disabled="disabled"
+    class="inline-flex h-14 items-center justify-center px-6 font-bold uppercase transition-colors duration-200 ease-in focus:outline-focus"
+    :class="[
+      variant === 'primary' &&
+        'bg-coral-300 hover:bg-coral-400 hover:text-white',
+      variant === 'secondary' &&
+        'border border-dark hover:bg-dark hover:text-white',
+      variant === 'tertiary' && 'border border-coral-300 hover:bg-coral-300',
+      variant === 'transparent' && 'hover:bg-gray-100',
+      disabled &&
+        'cursor-not-allowed border-0 bg-gray-100 hover:bg-gray-100 hover:text-gray-500',
+    ]"
   >
     <slot />
-  </button>
+  </component>
 </template>
